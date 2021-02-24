bb0d6e7f718d6c3f38f4eaa8328701ece4404717
bluegreenmagick <bluegreenmagick@gmail.com>
2021-02-24 11:09:43 +0900

    use StudyDeck to choose deck

--- src/ankihub/main.py
+++ src/ankihub/main.py
@@ -1,19 +1,16 @@
 from aqt import mw
 from aqt.qt import *
+from aqt.studydeck import StudyDeck
 
 from .sync import upload_deck
 
 
 def on_upload():
-    decks = mw.col.decks.all_names_and_ids(skip_empty_default=True)
-    deck_names = [deck.name for deck in decks]
-    window = QWidget(mw)
-    deck_name, ok = QInputDialog.getItem(
-        window, "AnkiHub", "Which deck do you want to upload?", deck_names, 0, False)
-    if ok and deck_name:
-        idx = deck_names.index(deck_name)
-        did = decks[idx].id
-        upload_deck(did)
+    deck_name = StudyDeck(mw, title="ankihub").name
+    if not deck_name:
+        return
+    did = mw.col.decks.id(deck_name)
+    upload_deck(did)
 
 
 def add_menu():
