1e8d3035a345cffad15ba727e07d2855747accaf
bluegreenmagick <bluegreenmagick@gmail.com>
2021-02-25 17:19:11 +0900

    add filtered deck test to get_note_types_in_deck

--- src/tests/test_sync.py
+++ src/tests/test_sync.py
@@ -1,5 +1,5 @@
 from contextlib import contextmanager
-from typing import Generator, Tuple
+from typing import Generator, List
 
 from anki.collection import Collection
 from anki.models import NoteType
@@ -10,6 +10,18 @@ from anki.notes import Note
 def add_deck(col: Collection) -> Generator[int, None, None]:
     "The deck with all its cards are removed afterwards"
     did = col.decks.id("AnkiHub Deck")
+    try:
+        yield did
+    finally:
+        col.decks.rem(did)
+
+
+@contextmanager
+def add_filtered_deck(col: Collection, search: str) -> Generator[int, None, None]:
+    did = col.decks.new_filtered("AnkiHub Filtered")
+    deck = col.decks.get(did)
+    deck["terms"] = [[search, 100, 1]]
+    col.decks.save(deck)
     try:
         yield did
     finally:
@@ -32,7 +44,7 @@ def add_note_type(col: Collection, name="AnkiHub") -> Generator[int, None, None]
         col.models.rem(ah_note_type)
 
 
-def add_cloze_notes(col: Collection, mid: int, did: int):
+def add_cloze_notes(col: Collection, mid: int, did: int, card_count: int = 10) -> List[Note]:
     "This function assumes note type is cloze type, and has field 'Text' and 'Extra'."
 
     def create_note(text, extra):
@@ -42,19 +54,21 @@ def add_cloze_notes(col: Collection, mid: int, did: int):
         note["Extra"] = extra
         return note
 
-    for i in range(10):
+    notes = []
+    for i in range(card_count):
         note = create_note("cloze {{c1::abc}}" + str(i), "extra text")
         col.add_note(note, did)
+        notes.append(note)
+    return notes
 
 
 def test_get_note_types_in_deck(col: Collection):
     from ankihub.sync import get_note_types_in_deck
-
     with add_deck(col) as did:
         with add_note_type(col, "AnkiHub1") as mid1:
             with add_note_type(col, "AnkiHub2") as mid2:
                 with add_note_type(col, "AnkiHub3") as mid3:
-                    add_cloze_notes(col, mid1, did)
+                    notes1 = add_cloze_notes(col, mid1, did, card_count=1)
                     add_cloze_notes(col, mid2, did)
                     add_cloze_notes(col, mid3, 1)  # default deck
 
@@ -62,3 +76,11 @@ def test_get_note_types_in_deck(col: Collection):
                     assert mid1 in mids
                     assert mid2 in mids
                     assert len(mids) == 2
+
+                    # Test for notes in filtered deck
+                    nid = notes1[0].id
+                    with add_filtered_deck(col, f"nid:{nid}") as fdid:
+                        mids = get_note_types_in_deck(did)
+                        assert mid1 in mids
+                        assert mid2 in mids
+                        assert len(mids) == 2
