0b97eb7c4d4c253c3f02f83862704ebb3d6896a1
Andrew Sanchez <inbox.asanchez@gmail.com>
2021-03-13 13:32:27 -0700

    Override the fixture here.

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,8 +1,17 @@
+from typing import Iterator
+
 import pytest
 
 from aqt.main import AnkiQt
 
-from pytest_anki import profile_loaded, AnkiSession
+from pytest_anki import profile_loaded, AnkiSession, anki_running
+
+
+@pytest.fixture(scope="session")
+def anki_session(request) -> Iterator[AnkiSession]:
+    param = getattr(request, "param", None)
+    with anki_running() if not param else anki_running(**param) as session:
+        yield session
 
 
 @pytest.fixture(scope="session")
