e4a932c32628f89770211dd8d530b9b17eef6894
Andrew Sanchez <inbox.asanchez@gmail.com>
2021-03-13 16:32:57 -0700

    Convert ctx manager to a pytest data factory fixture.

--- src/tests/test_sync.py
+++ src/tests/test_sync.py
@@ -1,9 +1,10 @@
 import copy
 from contextlib import contextmanager
-from typing import Generator, List
+from typing import Generator, List, Iterable
+
+import pytest
 
 from anki.collection import Collection
-from anki.models import NoteType
 from anki.notes import Note
 
 
@@ -29,20 +30,33 @@ def add_filtered_deck(col: Collection, search: str) -> Generator[int, None, None
         col.decks.rem(did)
 
 
-@contextmanager
-def add_note_type(col: Collection, name="AnkiHub") -> Generator[int, None, None]:
-    "The note type with all its cards are removed afterwards"
-    cloze_note_type = col.models.byName("Cloze")
-    ah_note_type = col.models.copy(cloze_note_type)
-    ah_note_type["name"] = name
-    extra_field = col.models.new_field("Extra")
-    col.models.add_field(ah_note_type, extra_field)
-    col.models.add(ah_note_type)
-    mid = col.models.id_for_name(name)
-    try:
-        yield mid
-    finally:
-        col.models.rem(ah_note_type)
+@pytest.fixture
+def add_note_types():
+    """The note type with all its cards are removed afterwards."""
+
+    notes = []
+    col_ = None
+
+    def _add_note_types(col: Collection, names: Iterable[str]):
+        nonlocal col_
+        col_ = col
+        mids = []
+        for name in names:
+            cloze_note_type = col.models.byName("Cloze")
+            ah_note_type = col.models.copy(cloze_note_type)
+            ah_note_type["name"] = name
+            extra_field = col.models.new_field("Extra")
+            col.models.add_field(ah_note_type, extra_field)
+            col.models.add(ah_note_type)
+            mid = col.models.id_for_name(name)
+            mids.append(mid)
+            notes.append(ah_note_type)
+        return mids
+
+    yield _add_note_types
+
+    for note in notes:
+        col_.models.rem(note)
 
 
 def add_cloze_notes(
@@ -65,66 +79,65 @@ def add_cloze_notes(
     return notes
 
 
-def test_get_note_types_in_deck(col: Collection):
+def test_get_note_types_in_deck(col: Collection, add_note_types):
     from ankihub.sync import get_note_types_in_deck
 
     with add_deck(col) as did:
-        with add_note_type(col, "AnkiHub1") as mid1:
-            with add_note_type(col, "AnkiHub2") as mid2:
-                with add_note_type(col, "AnkiHub3") as mid3:
-                    notes1 = add_cloze_notes(col, mid1, did, card_count=1)
-                    add_cloze_notes(col, mid2, did)
-                    add_cloze_notes(col, mid3, 1)  # default deck
+        names = ["AnkiHub1", "AnkiHub2", "AnkiHub3"]
+        mid1, mid2, mid3 = add_note_types(col, names)
+        notes1 = add_cloze_notes(col, mid1, did, card_count=1)
+        add_cloze_notes(col, mid2, did)
+        add_cloze_notes(col, mid3, 1)  # default deck
 
-                    mids = get_note_types_in_deck(did)
-                    assert mid1 in mids
-                    assert mid2 in mids
-                    assert len(mids) == 2
+        mids = get_note_types_in_deck(did)
+        assert mid1 in mids
+        assert mid2 in mids
+        assert len(mids) == 2
 
-                    # Test for notes in filtered deck
-                    nid = notes1[0].id
-                    with add_filtered_deck(col, f"nid:{nid}") as fdid:
-                        mids = get_note_types_in_deck(did)
-                        assert mid1 in mids
-                        assert mid2 in mids
-                        assert len(mids) == 2
+        # Test for notes in filtered deck
+        nid = notes1[0].id
+        with add_filtered_deck(col, f"nid:{nid}") as fdid:
+            mids = get_note_types_in_deck(did)
+            assert mid1 in mids
+            assert mid2 in mids
+            assert len(mids) == 2
 
 
-def test_note_type_preparations(col: Collection):
+def test_note_type_preparations(col: Collection, add_note_types):
     from ankihub.sync import has_ankihub_field, prepare_note_types
     from ankihub.consts import FIELD_NAME
 
-    with add_note_type(col, "AnkiHub1") as mid:
-        note_type = col.models.get(mid)
-        assert has_ankihub_field(note_type) == False
+    mid = add_note_types(col, ["AnkiHub1"]).pop()
+    note_type = col.models.get(mid)
+    assert has_ankihub_field(note_type) == False
 
-        prev_templs = copy.deepcopy(note_type["tmpls"])
-        prepare_note_types([note_type])
+    prev_templs = copy.deepcopy(note_type["tmpls"])
+    prepare_note_types([note_type])
 
-        # prepare_note_types added the field
-        note_type = col.models.get(mid)
-        assert has_ankihub_field(note_type) == True
-        assert any(field["name"] == FIELD_NAME for field in note_type["flds"])
+    # prepare_note_types added the field
+    note_type = col.models.get(mid)
+    assert has_ankihub_field(note_type) == True
+    assert any(field["name"] == FIELD_NAME for field in note_type["flds"])
 
-        # prepare_note_types modified the template
-        templs = note_type["tmpls"]
-        assert len(prev_templs) == len(templs)
-        for i, templ in enumerate(templs):
-            assert templ != prev_templs[i]["afmt"]
+    # prepare_note_types modified the template
+    templs = note_type["tmpls"]
+    assert len(prev_templs) == len(templs)
+    for i, templ in enumerate(templs):
+        assert templ != prev_templs[i]["afmt"]
 
 
-def test_get_unprepared_note_types(col: Collection):
+def test_get_unprepared_note_types(col: Collection, add_note_types):
     from ankihub.sync import get_unprepared_note_types, prepare_note_types
 
-    with add_note_type(col, "AnkiHub1") as mid1:
-        with add_note_type(col, "AnkiHub2") as mid2:
-            note_types = get_unprepared_note_types([mid1, mid2])
-            assert len(note_types) == 2
-            assert note_types[0]["id"] == mid1
-            assert note_types[1]["id"] == mid2
+    names = ["AnkiHub1", "AnkiHub2"]
+    mid1, mid2 = add_note_types(col, names)
+    note_types = get_unprepared_note_types([mid1, mid2])
+    assert len(note_types) == 2
+    assert note_types[0]["id"] == mid1
+    assert note_types[1]["id"] == mid2
 
-            # It shouldn't return mid1 note type after preparing
-            prepare_note_types([note_types[0]])
-            note_types = get_unprepared_note_types([mid1, mid2])
-            assert len(note_types) == 1
-            assert note_types[0]["id"] == mid2
+    # It shouldn't return mid1 note type after preparing
+    prepare_note_types([note_types[0]])
+    note_types = get_unprepared_note_types([mid1, mid2])
+    assert len(note_types) == 1
+    assert note_types[0]["id"] == mid2
