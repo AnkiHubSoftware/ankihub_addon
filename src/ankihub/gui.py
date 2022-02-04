d4ab9010a7505e807dedcd0cd726e83c55475a97
Andrew Sanchez <inbox.asanchez@gmail.com>
2022-02-04 16:27:41 -0700

    Port main to gui.

--- src/ankihub/gui.py
+++ src/ankihub/gui.py
@@ -3,6 +3,8 @@ from aqt.qt import QMenu, QAction, qconnect
 from aqt.studydeck import StudyDeck
 
 from .register_decks import create_shared_deck
+from .service import Config
+from .dialog import AnkiHubLogin
 
 
 def create_shared_deck_action() -> None:
@@ -27,3 +29,20 @@ def add_ankihub_menu() -> None:
     _create_shared_deck_action = QAction("Upload Deck", parent=ankihub_menu)
     qconnect(_create_shared_deck_action.triggered, create_shared_deck_action)
     ankihub_menu.addAction(_create_shared_deck_action)
+    if Config().isAuthenticated():
+        sign_out_button = QAction("Sign out", mw)
+        sign_out_button.triggered.connect(signout)
+        ankihub_menu.addAction(sign_out_button)
+    else:
+        sign_in_button = QAction("Sign in", mw)
+        sign_in_button.triggered.connect(show_sign_in_screen)
+        ankihub_menu.addAction(sign_in_button)
+
+
+def signout() -> None:
+    Config().signout()
+
+
+def show_sign_in_screen() -> None:
+    global __window
+    __window = AnkiHubLogin()
