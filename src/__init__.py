6d56f04426fbad2f3be6f5c0641e03e28222225f
Andrew Sanchez <inbox.asanchez@gmail.com>
2022-02-05 16:06:12 -0700

    Fix import.

--- src/__init__.py
+++ src/__init__.py
@@ -5,6 +5,6 @@ __all__ = ["gui"]
 SKIP_INIT = os.getenv("SKIP_INIT", False)
 if not SKIP_INIT:
     # Explicit is better than implicit. (⌐⊙_⊙)
-    from ankihub import entry_point
+    from .ankihub import entry_point
 
     entry_point.run()
