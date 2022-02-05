4c31d77619055f7fcdb22bf7c5b6220ccd31a5d3
Andrew Sanchez <inbox.asanchez@gmail.com>
2022-02-05 16:32:06 -0700

    Simplify login and reorder.

--- src/ankihub/gui.py
+++ src/ankihub/gui.py
@@ -7,6 +7,93 @@ from .ankihub_client import AnkiHubClient
 from .register_decks import create_shared_deck
 
 
+class AnkiHubLogin(QWidget):
+    def __init__(self):
+        super(AnkiHubLogin, self).__init__()
+        self.results = None
+        self.thread = None
+        self.box_top = QVBoxLayout()
+        self.box_upper = QHBoxLayout()
+        self.box_left = QVBoxLayout()
+        self.box_right = QVBoxLayout()
+        self.bottom_box_section = QHBoxLayout()
+
+        # Username
+        self.username_box = QHBoxLayout()
+        self.username_box_label = QLabel("Username:")
+        self.username_box_text = QLineEdit("", self)
+        self.username_box_text.setMinimumWidth(300)
+        self.username_box.addWidget(self.username_box_label)
+        self.username_box.addWidget(self.username_box_text)
+        self.box_left.addLayout(self.username_box)
+
+        # Password
+        self.password_box = QHBoxLayout()
+        self.password_box_label = QLabel("Password:")
+        self.password_box_text = QLineEdit("", self)
+        self.password_box_text.setMinimumWidth(300)
+        self.password_box.addWidget(self.password_box_label)
+        self.password_box.addWidget(self.password_box_text)
+        self.box_left.addLayout(self.password_box)
+
+        # Login
+        self.login_button = QPushButton("Login", self)
+        self.bottom_box_section.addWidget(self.login_button)
+        self.login_button.clicked.connect(self.login)
+
+        self.box_left.addLayout(self.bottom_box_section)
+
+        # Add left and right layouts to upper
+        self.box_upper.addLayout(self.box_left)
+        self.box_upper.addSpacing(20)
+        self.box_upper.addLayout(self.box_right)
+
+        self.label_results = QLabel(
+            """
+            \r\n<i>Use your AnkiHub username and password to log in.</i>
+            """
+        )
+
+        # Add all widgets to top layout.
+        self.box_top.addLayout(self.box_upper)
+        self.box_top.addWidget(self.label_results)
+        self.box_top.addStretch(1)
+        self.setLayout(self.box_top)
+
+        self.setMinimumWidth(500)
+        self.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
+        self.setWindowTitle("Login to AnkiHub.")
+        self.show()
+
+    def login(self):
+        self.label_results.setText("Waiting...")
+        # grab input
+        username = self.username_box_text.text()
+        password = self.password_box_text.text()
+        if username == "" or password == "":
+            self.label_results.setText(
+                "Oops! You forgot to put in a username or password!"
+            )
+
+        ankihub_client = AnkiHubClient()
+        token = ankihub_client.authenitcateUserGetToken(
+            url="auth-token/", data={"username": username, "password": password}
+        )
+        if token:
+            self.label_results.setText("You are now logged into AnkiHub.")
+        else:
+            self.label_results.setText(
+                "AnkiHub login failed.  Please make sure your username and "
+                "password are correct for AnkiHub."
+                )
+
+    @classmethod
+    def display_login(cls):
+        global __window
+        __window = cls()
+        return __window
+
+
 def create_shared_deck_action() -> None:
     diag = StudyDeck(
         mw,
@@ -32,108 +119,3 @@ def add_ankihub_menu() -> None:
     sign_in_button = QAction("Sign in", mw)
     sign_in_button.triggered.connect(AnkiHubLogin.display_login)
     ankihub_menu.addAction(sign_in_button)
-
-
-class AnkiHubLogin(QWidget):
-    def __init__(self):
-        super(AnkiHubLogin, self).__init__()
-        self.results = None
-        self.thread = None
-        self.box_top = QVBoxLayout()
-        self.box_upper = QHBoxLayout()
-        self.box_left = QVBoxLayout()
-        self.box_right = QVBoxLayout()
-        self.connect_box = QHBoxLayout()
-        self.bottom_box_section = QHBoxLayout()
-
-        # Username
-        self.username_box = QHBoxLayout()
-        self.username_box_label = QLabel("Username:")
-        self.username_box_text = QLineEdit("", self)
-        self.username_box_text.setMinimumWidth(300)
-        self.username_box.addWidget(self.username_box_label)
-        self.username_box.addWidget(self.username_box_text)
-        self.box_left.addLayout(self.username_box)
-
-        # Password
-        self.password_box = QHBoxLayout()
-        self.password_box_label = QLabel("Password:")
-        self.password_box_text = QLineEdit("", self)
-        self.password_box_text.setMinimumWidth(300)
-        self.password_box.addWidget(self.password_box_label)
-        self.password_box.addWidget(self.password_box_text)
-        self.box_left.addLayout(self.password_box)
-
-        # Connection
-        self.connect_box_button = QPushButton("Connect", self)
-        self.connect_box.addStretch(1)
-        self.connect_box.addWidget(self.connect_box_button)
-        self.connect_box_button.clicked.connect(self.test_connection)
-
-        # Upload your collection to AnkiHub.
-        self.upload_but = QPushButton("Upload", self)
-        self.bottom_box_section.addWidget(self.upload_but)
-        self.upload_but.clicked.connect(self.upload_button)
-
-        # Download your collection from ankihub
-        self.upload_but = QPushButton("Download", self)
-        self.bottom_box_section.addWidget(self.upload_but)
-        self.upload_but.clicked.connect(self.upload_button)
-
-        self.box_left.addLayout(self.bottom_box_section)
-        self.box_right.addLayout(self.connect_box)
-
-        # Add left and right layouts to upper
-        self.box_upper.addLayout(self.box_left)
-        self.box_upper.addSpacing(20)
-        self.box_upper.addLayout(self.box_right)
-
-        self.label_results = QLabel(
-            """
-            \r\n<i>Use the same username and password
-            from AnkiHub to be able to sync</i>
-            """
-        )
-
-        # Add all widgets to top layout.
-        self.box_top.addLayout(self.box_upper)
-        self.box_top.addWidget(self.label_results)
-        self.box_top.addStretch(1)
-        self.setLayout(self.box_top)
-
-        self.setMinimumWidth(500)
-        self.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
-        self.setWindowTitle("AnkiHub connection setup")
-        self.show()
-
-    def upload_button(self):
-        self.label_results.setText(
-            """We've started uploading your collection.
-            You will be notified on completion. \n"""
-        )
-
-    def test_connection(self):
-        self.label_results.setText("Waiting...")
-        # grab input
-        username = self.username_box_text.text()
-        password = self.password_box_text.text()
-        if username == "" or password == "":
-            self.label_results.setText(
-                "Oops! You forgot to put in a username or password!"
-            )
-
-        ankihub_client = AnkiHubClient()
-        token = ankihub_client.authenitcateUserGetToken(
-            url="auth-token/", data={"username": username, "password": password}
-        )
-
-        if token:
-            self.label_results.setText("We've connected securely!: " + token)
-        else:
-            self.label_results.setText("There seems to have been an error!")
-
-    @classmethod
-    def display_login(cls):
-        global __window
-        __window = cls()
-        return __window
