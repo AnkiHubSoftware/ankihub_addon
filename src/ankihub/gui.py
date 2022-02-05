d175729de1da6d1d44888f3b025ec5f1bdef3e4c
Andrew Sanchez <inbox.asanchez@gmail.com>
2022-02-05 16:12:47 -0700

    Clean up login gui.

--- src/ankihub/gui.py
+++ src/ankihub/gui.py
@@ -39,77 +39,55 @@ class AnkiHubLogin(QWidget):
         super(AnkiHubLogin, self).__init__()
         self.results = None
         self.thread = None
-        self.init_gui()
-
-    # create GUI skeleton
-    def init_gui(self):
-
         self.box_top = QVBoxLayout()
         self.box_upper = QHBoxLayout()
-
-        # LEFT SECTION
         self.box_left = QVBoxLayout()
+        self.box_right = QVBoxLayout()
+        self.connect_box = QHBoxLayout()
+        self.bottom_box_section = QHBoxLayout()
 
-        # username
+        # Username
         self.username_box = QHBoxLayout()
         self.username_box_label = QLabel("Username:")
         self.username_box_text = QLineEdit("", self)
         self.username_box_text.setMinimumWidth(300)
-
         self.username_box.addWidget(self.username_box_label)
         self.username_box.addWidget(self.username_box_text)
-
-        # add layouts to left
         self.box_left.addLayout(self.username_box)
 
-        # password
+        # Password
         self.password_box = QHBoxLayout()
         self.password_box_label = QLabel("Password:")
         self.password_box_text = QLineEdit("", self)
         self.password_box_text.setMinimumWidth(300)
-
         self.password_box.addWidget(self.password_box_label)
         self.password_box.addWidget(self.password_box_text)
-
-        # add layouts to left
         self.box_left.addLayout(self.password_box)
 
-        # RIGHT SECTION
-        self.box_right = QVBoxLayout()
-
-        # code (import set) button
-        self.connect_box = QHBoxLayout()
+        # Connection
         self.connect_box_button = QPushButton("Connect", self)
         self.connect_box.addStretch(1)
         self.connect_box.addWidget(self.connect_box_button)
-        self.connect_box_button.clicked.connect(self.onClickConnnect)
+        self.connect_box_button.clicked.connect(self.test_connection)
 
-        self.bottom_box_section = QHBoxLayout()
-
-        # Upload your collection to ankihub
+        # Upload your collection to AnkiHub.
         self.upload_but = QPushButton("Upload", self)
         self.bottom_box_section.addWidget(self.upload_but)
-        self.upload_but.clicked.connect(self.uploadBut)
+        self.upload_but.clicked.connect(self.upload_button)
 
         # Download your collection from ankihub
         self.upload_but = QPushButton("Download", self)
         self.bottom_box_section.addWidget(self.upload_but)
-        self.upload_but.clicked.connect(self.uploadBut)
-
-        # Signout from AnkiHub
-        self.upload_but = QPushButton("Signout", self)
-        self.bottom_box_section.addWidget(self.upload_but)
-        self.upload_but.clicked.connect(self.uploadBut)
+        self.upload_but.clicked.connect(self.upload_button)
 
         self.box_left.addLayout(self.bottom_box_section)
         self.box_right.addLayout(self.connect_box)
 
-        # add left and right layouts to upper
+        # Add left and right layouts to upper
         self.box_upper.addLayout(self.box_left)
         self.box_upper.addSpacing(20)
         self.box_upper.addLayout(self.box_right)
 
-        # results label
         self.label_results = QLabel(
             """
             \r\n<i>Use the same username and password
@@ -117,26 +95,24 @@ class AnkiHubLogin(QWidget):
             """
         )
 
-        # add all widgets to top layout
+        # Add all widgets to top layout.
         self.box_top.addLayout(self.box_upper)
         self.box_top.addWidget(self.label_results)
         self.box_top.addStretch(1)
         self.setLayout(self.box_top)
 
-        # go, baby go!
         self.setMinimumWidth(500)
         self.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
         self.setWindowTitle("AnkiHub connection setup")
         self.show()
 
-    def uploadBut(self):
+    def upload_button(self):
         self.label_results.setText(
             """We've started uploading your collection.
             You will be notified on completion. \n"""
         )
-        # uploadToMFC()
 
-    def onClickConnnect(self):
+    def test_connection(self):
         self.label_results.setText("Waiting...")
         # grab input
         username = self.username_box_text.text()
