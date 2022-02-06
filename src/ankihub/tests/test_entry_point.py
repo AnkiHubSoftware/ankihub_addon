9744025a5331b3279de40ef6ac334e2d5a395137
Andrew Sanchez <inbox.asanchez@gmail.com>
2022-02-06 10:48:38 -0700

    Fix import.

--- src/ankihub/tests/test_entry_point.py
+++ src/ankihub/tests/test_entry_point.py
@@ -2,7 +2,7 @@ from pytest_anki import AnkiSession
 
 
 def test_entry_point(anki_session: AnkiSession):
-    from anki_dev.qt.aqt.main import AnkiQt
+    from aqt.main import AnkiQt
 
     from src.ankihub import entry_point
 
