a551e1d9b2ff994803385e57eea264105bb7c12f
Andrew Sanchez <inbox.asanchez@gmail.com>
2022-02-14 13:09:52 -0700

    style.

--- src/ankihub/tests/test_editor.py
+++ src/ankihub/tests/test_editor.py
@@ -1,16 +1,17 @@
 from pytest_anki import AnkiSession
 
 
-
 def test_editor(anki_session_with_addon: AnkiSession):
     from aqt.editor import Editor
+
+    from ankihub.constants import AnkiHubCommands
     from ankihub.gui.editor import (
         on_ankihub_button_press,
         on_bridge_command,
         on_select_command,
         setup,
     )
-    from ankihub.constants import AnkiHubCommands
+
     editor = setup()
     assert editor.ankihub_command == "Suggest a change"
     on_select_command(editor, AnkiHubCommands.NEW.value)
@@ -18,4 +19,4 @@ def test_editor(anki_session_with_addon: AnkiSession):
     on_bridge_command(editor, f"ankihub:{AnkiHubCommands.CHANGE.value}", lambda: None)
     assert editor.ankihub_command == "Suggest a change"
     response = on_ankihub_button_press(editor)
-    # TODO assert response 
+    # TODO assert response
