cc4b1806af4fc544838313c9dd9109d7db5c5d64
Andrew Sanchez <inbox.asanchez@gmail.com>
2022-02-21 15:30:32 -0700

    isort.

--- ankihub/gui/menu.py
+++ ankihub/gui/menu.py
@@ -1,6 +1,5 @@
 from ankihub.ankihub_client import AnkiHubClient
 from ankihub.register_decks import create_collaborative_deck
-
 from aqt import mw
 from aqt.qt import QAction, QMenu, qconnect
 from aqt.studydeck import StudyDeck
