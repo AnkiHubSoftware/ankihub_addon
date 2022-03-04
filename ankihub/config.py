5f1dbfa4056c811494cefd9d7f31110e9a11de44
Pedro <pedrobeethoven8@gmail.com>
2022-03-04 19:39:51 -0300

    WIP: Update AnkiHubClient

--- ankihub/config.py
+++ ankihub/config.py
@@ -1,20 +1,35 @@
+from datetime import datetime, timezone
+
 from aqt import mw
 
 
 class Config:
     def __init__(self):
         self.config = mw.addonManager.getConfig("ankihub")
-        self.user = self.config.get("user")
-        self.token = self.user.get("token")
+        self.base_url = self.config.get("baseUrl")
+        self.token = self.config.get("token")
+        self.last_sync = self.config.get("lastSync")
 
-    def is_authenticated(self) -> bool:
-        return True if self.token else False
+    def get_base_url(self) -> str:
+        return self.base_url
 
-    def signout(self):
-        default = mw.addonManager.addonConfigDefaults(__name__)
-        mw.addonManager.writeConfig(__name__, default)
-
-    def write_token(self, token: str) -> None:
-        # TODO needs test
+    def save_token(self, token: str):
         self.token = token
+        self.config["token"] = token
         mw.addonManager.writeConfig(__name__, self.config)
+
+    def get_token(self) -> str:
+        if self.token:
+            return self.token
+        return None
+
+    def save_last_sync(self):
+        date_object = datetime.now(tz=timezone.utc)
+        date_time_str = datetime.strftime(date_object, "%Y-%m-%dT%H:%M:%S.%f%z")
+        self.config["lastSync"] = date_time_str
+        mw.addonManager.writeConfig(__name__, self.config)
+
+    def get_last_sync(self):
+        if self.last_sync:
+            return self.last_sync
+        return None
