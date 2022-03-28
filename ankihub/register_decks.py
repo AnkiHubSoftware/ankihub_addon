a78c33d4828676bf703a7f23afcecb30cf8e4a02
Andrew Sanchez <inbox.asanchez@gmail.com>
2022-03-28 16:03:26 -0700

    Remove to-do.

--- ankihub/register_decks.py
+++ ankihub/register_decks.py
@@ -82,9 +82,6 @@ def upload_deck(did: int) -> None:
     out_dir = pathlib.Path(tempfile.mkdtemp())
     out_file = str(out_dir / f"export-{deck_uuid}.apkg")
     exporter.exportInto(out_file)
-    # TODO First we need to send a request to upload the apkg to s3
-    #  The ankihub request just needs to post the name of the apkg
-    #  ("export-{deck_uuid}.apkg")
     ankihub_client = AnkiHubClient()
     response = ankihub_client.upload_deck(f"{deck_name}.apkg")
     tooltip("Deck Uploaded to AnkiHub")
