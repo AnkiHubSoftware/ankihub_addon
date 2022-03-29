4eb1d1d6f5b310d1238f9c344af98683ff65c6d4
Andrew Sanchez <inbox.asanchez@gmail.com>
2022-03-29 09:37:53 -0700

    Get current deck id.

--- ankihub/gui/tests/test_editor.py
+++ ankihub/gui/tests/test_editor.py
@@ -9,8 +9,10 @@ def test_editor(anki_session_with_addon: AnkiSession, monkeypatch, requests_mock
     from ankihub.constants import AnkiHubCommands
 
     anki_editor = editor.setup()
+    # Check the default command.
     assert anki_editor.ankihub_command == "Suggest a change"
     editor.on_select_command(anki_editor, AnkiHubCommands.NEW.value)
+    # Check that the command was updated.
     assert anki_editor.ankihub_command == "Suggest a new note"
     editor.ankihub_message_handler(
         (False, None),
@@ -20,14 +22,16 @@ def test_editor(anki_session_with_addon: AnkiSession, monkeypatch, requests_mock
     assert anki_editor.ankihub_command == "Suggest a change"
     # Patch the editor so that it has the note attribute, which it will have when
     # the editor is actually instantiated during an Anki Desktop session.
-    mock = MagicMock()
-    anki_editor.note = mock
-    mock.id = 123
-    mock.data = {
+    anki_editor.note = MagicMock()
+    anki_editor.mw = MagicMock()
+    anki_editor.note.id = 123
+    anki_editor.note.data = {
         "tags": ["test"],
         "fields": [{"name": "abc", "order": 0, "value": "abc changed"}],
     }
 
-    requests_mock.post(f"{API_URL_BASE}/notes/{mock.id}/suggestion/", status_code=201)
+    requests_mock.post(
+        f"{API_URL_BASE}/notes/{anki_editor.note.id}/suggestion/", status_code=201
+    )
     response = editor.on_ankihub_button_press(anki_editor)
     assert response.status_code == 201
