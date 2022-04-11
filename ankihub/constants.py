86dd3b6f88d03da02868ab96691dad353dcd631e
Andrew Sanchez <inbox.asanchez@gmail.com>
2022-04-11 09:22:45 -0700

    Rename field.

--- ankihub/constants.py
+++ ankihub/constants.py
@@ -4,7 +4,7 @@ from enum import Enum
 URL_BASE = "https://hub.ankipalace.com/"
 API_URL_BASE = "http://localhost:8000/api"
 URL_VIEW_NOTE = URL_BASE + "note/"
-ANKIHUB_NOTE_TYPE_FIELD_NAME = "AnkiHub ID (hidden)"
+ANKIHUB_NOTE_TYPE_FIELD_NAME = "AnkiHub ID"
 ADDON_PATH = pathlib.Path(__file__).parent.absolute()
 ICONS_PATH = ADDON_PATH / "icons"
 
