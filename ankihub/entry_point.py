490d37aa9bdcaa5d6c14f62d2471591c560b5248
Andrew Sanchez <inbox.asanchez@gmail.com>
2022-04-21 09:59:15 -0700

    Sync when profile opens.

--- ankihub/entry_point.py
+++ ankihub/entry_point.py
@@ -3,12 +3,13 @@ from aqt import gui_hooks, mw
 
 from .gui import editor
 from .gui.menu import setup_ankihub_menu
-from .utils import hide_ankihub_field_in_editor
+from .utils import hide_ankihub_field_in_editor, sync_with_ankihub
 
 
 def run():
-    """This is the function that will be run in __init__.py when Anki starts."""
+    """Call this function in __init__.py when Anki starts."""
     gui_hooks.editor_will_load_note.append(hide_ankihub_field_in_editor)
+    gui_hooks.profile_did_open.append(sync_with_ankihub)
     setup_ankihub_menu()
     editor.setup()
     return mw
