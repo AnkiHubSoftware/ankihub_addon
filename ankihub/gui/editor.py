d7b2fc33ec7379302813cd7b3e83c62884d162e6
Andrew Sanchez <inbox.asanchez@gmail.com>
2022-05-02 13:31:06 -0700

    Include AnkiHub ID.

--- ankihub/gui/editor.py
+++ ankihub/gui/editor.py
@@ -53,6 +53,7 @@ def on_ankihub_button_press(editor: Editor):
         response = client.create_new_note_suggestion(
             deck_id=deck_id,
             anki_id=editor.note.id,
+            ankihub_id=ankihub_id,
             fields=fields,
             tags=tags,
         )
