7d3697d035c311a863644d7bbfd065ef62dc48a0
Andrew Sanchez <inbox.asanchez@gmail.com>
2022-06-12 14:08:58 -0700

    Use dictionary config.

--- ankihub/__init__.py
+++ ankihub/__init__.py
@@ -1,24 +1,12 @@
-import logging
+import logging.config
 import os
-import sys
-from logging.handlers import RotatingFileHandler
-from pathlib import Path
+from . import settings
 
+logging.config.dictConfig(settings.LOGGING)
 LOGGER = logging.getLogger("ankihub")
-LOGGER.setLevel(logging.DEBUG)
-
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-
-stdout_handler = logging.StreamHandler(stream=sys.stdout)
-stdout_handler.setFormatter(formatter)
-LOGGER.addHandler(stdout_handler)
-
-LOG_FILENAME = Path(__file__).parent / "user_files/log.txt"
-file_handler = RotatingFileHandler(LOG_FILENAME, maxBytes=2000, backupCount=5)
-file_handler.setFormatter(formatter)
-LOGGER.addHandler(file_handler)
 
 SKIP_INIT = os.getenv("SKIP_INIT", False)
+LOGGER.debug(f"SKIP_INIT: {SKIP_INIT}")
 if not SKIP_INIT:
     # Explicit is better than implicit. (⌐⊙_⊙)
     from . import entry_point
