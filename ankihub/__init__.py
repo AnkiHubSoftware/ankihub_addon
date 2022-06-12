a643a655628354987808a92b608d1f7cf1f39df6
RisingOrange <jakub.fidler@protonmail.com>
2022-06-12 15:39:15 +0200

    Write logs to a file #30

--- ankihub/__init__.py
+++ ankihub/__init__.py
@@ -1,14 +1,22 @@
 import logging
 import os
 import sys
+from logging.handlers import RotatingFileHandler
+from pathlib import Path
 
 LOGGER = logging.getLogger("ankihub")
 LOGGER.setLevel(logging.DEBUG)
-stdout_handler = logging.StreamHandler(stream=sys.stdout)
+
 formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
+stdout_handler = logging.StreamHandler(stream=sys.stdout)
 stdout_handler.setFormatter(formatter)
 LOGGER.addHandler(stdout_handler)
 
+LOG_FILENAME = Path(__file__).parent / "user_files/log.txt"
+file_handler = RotatingFileHandler(LOG_FILENAME, maxBytes=2000, backupCount=5)
+file_handler.setFormatter(formatter)
+LOGGER.addHandler(file_handler)
 
 SKIP_INIT = os.getenv("SKIP_INIT", False)
 if not SKIP_INIT:
