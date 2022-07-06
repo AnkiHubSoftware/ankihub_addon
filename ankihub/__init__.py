3eaf478a9e051dcf388da2078f02f12689478598
RisingOrange <jakub.fidler@protonmail.com>
2022-07-06 16:53:18 +0200

    Log exceptions

--- ankihub/__init__.py
+++ ankihub/__init__.py
@@ -44,5 +44,7 @@ if not SKIP_INIT:
         from . import entry_point
 
         mw = entry_point.run()
-    except:
+    except Exception as e:
+        # should we show exception to the user too?
+        LOGGER.exception(e)
         report_exception()
