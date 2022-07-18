199474617955cde5b885f02473de4752f10b2be4
RisingOrange <31575114+RisingOrange@users.noreply.github.com>
2022-07-19 00:04:59 +0200

    feat: bulk tag suggestions (#139)

--- ankihub/entry_point.py
+++ ankihub/entry_point.py
@@ -2,7 +2,7 @@
 from aqt import gui_hooks, mw
 
 from . import LOGGER
-from .gui import editor
+from .gui import browser, editor
 from .gui.menu import setup_ankihub_menu
 from .sync import sync_on_profile_open
 
@@ -10,9 +10,16 @@ from .sync import sync_on_profile_open
 def run():
     """Call this function in __init__.py when Anki starts."""
     gui_hooks.profile_did_open.append(sync_on_profile_open)
+
     mw.addonManager.setWebExports(__name__, r"gui/web/.*")
+
     setup_ankihub_menu()
     LOGGER.debug("Set up AnkiHub menu.")
+
     editor.setup()
     LOGGER.debug("Set up editor.")
+
+    browser.setup()
+    LOGGER.debug("Set up browser.")
+
     return mw
