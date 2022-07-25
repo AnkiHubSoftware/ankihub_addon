c5481dfdfb151aedab5246f6cfbafd4856bf2398
RisingOrange <jakub.fidler@protonmail.com>
2022-07-25 18:08:16 +0200

    Edit update_media_import to add google api key

--- scripts/update_media_import.py
+++ scripts/update_media_import.py
@@ -1,9 +1,23 @@
+import os
 import shutil
+import subprocess
 from pathlib import Path
+
+google_api_key = os.getenv("GOOGLE_API_KEY")
 
 PROJECT_ROOT = Path(__file__).parent.parent
 MEDIA_IMPORT_SRC = PROJECT_ROOT / "media_import/src/addon"
 MEDIA_IMPORT_TARGET = PROJECT_ROOT / "ankihub/media_import"
 
+API_KEY_OBFUSCATE_SCRIPT = Path(__file__).parent / "google_api_obfuscate.py"
+
 shutil.rmtree(MEDIA_IMPORT_TARGET, ignore_errors=True)
 shutil.copytree(MEDIA_IMPORT_SRC, MEDIA_IMPORT_TARGET)
+subprocess.run(
+    [
+        "python3",
+        str(API_KEY_OBFUSCATE_SCRIPT),
+        google_api_key,
+        str(MEDIA_IMPORT_TARGET),
+    ]
+)
