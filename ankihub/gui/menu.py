af5957876fae3f0e41faadfe105b0e806c4264b2
RisingOrange <jakub.fidler@protonmail.com>
2022-07-25 16:49:01 +0200

    Try other import

--- ankihub/gui/menu.py
+++ ankihub/gui/menu.py
@@ -17,13 +17,12 @@ from aqt.studydeck import StudyDeck
 from aqt.utils import askUser, showInfo, showText, tooltip
 from requests import Response
 
-from .. import LOGGER
+from .. import LOGGER, media_import
 from ..addon_ankihub_client import AddonAnkiHubClient as AnkiHubClient
 from ..config import config
 from ..register_decks import create_collaborative_deck
 from ..sync import sync_deck_with_ankihub, sync_with_progress
 from .decks import SubscribedDecksDialog
-from ..media_import import open_import_dialog
 
 class AnkiHubLogin(QWidget):
     _window: Optional["AnkiHubLogin"] = None
@@ -218,7 +217,7 @@ def subscribe_to_deck_setup(parent):
 
 def import_media_setup(parent):
     q_action = QAction("🖼️ Import media", mw)
-    q_action.triggered.connect(open_import_dialog)
+    q_action.triggered.connect(media_import.open_import_dialog)
     parent.addAction(q_action)
 
 def sync_with_ankihub_setup(parent):
