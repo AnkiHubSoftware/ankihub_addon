379f57714b5b39a2fe65dcd9c4ea8e60f3989cb9
RisingOrange <jakub.fidler@protonmail.com>
2022-07-26 11:33:46 +0200

    Change MEDIA_IMPORT_SRC path in build.py

--- scripts/build.py
+++ scripts/build.py
@@ -6,7 +6,7 @@ from pathlib import Path
 google_api_key = os.getenv("GOOGLE_API_KEY")
 
 PROJECT_ROOT = Path(__file__).parent.parent
-MEDIA_IMPORT_SRC = PROJECT_ROOT / "media_import/src/addon"
+MEDIA_IMPORT_SRC = PROJECT_ROOT / "media_import/src/media_import"
 MEDIA_IMPORT_LIBS = MEDIA_IMPORT_SRC / "libs"
 MEDIA_IMPORT_TARGET = PROJECT_ROOT / "ankihub/media_import"
 MEDIA_IMPORT_REQUIREMENTS = PROJECT_ROOT / "media_import" / "requirements.txt"
@@ -14,17 +14,19 @@ MEDIA_IMPORT_REQUIREMENTS = PROJECT_ROOT / "media_import" / "requirements.txt"
 API_KEY_OBFUSCATE_SCRIPT = Path(__file__).parent / "google_api_obfuscate.py"
 
 subprocess.run("git submodule update --init --recursive", shell=True, cwd=PROJECT_ROOT)
-subprocess.run([
-    "python3",
-    "-m",
-    "pip",
-    "install",
-    "--no-deps",
-    "--target",
-    str(MEDIA_IMPORT_LIBS),
-    "-r",
-    str(MEDIA_IMPORT_REQUIREMENTS)
-])
+subprocess.run(
+    [
+        "python3",
+        "-m",
+        "pip",
+        "install",
+        "--no-deps",
+        "--target",
+        str(MEDIA_IMPORT_LIBS),
+        "-r",
+        str(MEDIA_IMPORT_REQUIREMENTS),
+    ]
+)
 
 shutil.rmtree(MEDIA_IMPORT_TARGET, ignore_errors=True)
 shutil.copytree(MEDIA_IMPORT_SRC, MEDIA_IMPORT_TARGET)
