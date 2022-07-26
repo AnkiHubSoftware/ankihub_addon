9f7b7303121e8f579cf3f43e0d6ad0b347e68cbf
RisingOrange <jakub.fidler@protonmail.com>
2022-07-26 12:10:52 +0200

    Edit release.sh

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,4 +1,8 @@
-find . -name __pycache__ -or -regex ".*.py[cod]" -or -name .DS_Store | xargs rm -rf
+set -e
+
 python3 scripts/generate_manifest.py
 python3 scripts/build.py
+
+cd ankihub
+find . -name __pycache__ -or -regex ".*.py[cod]" -or -name .DS_Store -or -name ".pytest_cache" | xargs rm -rf
 zip -r "../ankihub.ankiaddon" . -x ./tests\*
