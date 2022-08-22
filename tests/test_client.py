11b37675dbff25cd324dc134944de7e29eda2fe7
Andrew Sanchez <inbox.asanchez@gmail.com>
2022-08-21 19:51:40 -0700

    Correct assertions.

--- tests/test_client.py
+++ tests/test_client.py
@@ -11,8 +11,11 @@ def test_client_login_and_signout(monkeypatch):
     client = AnkiHubClient()
     credentials_data = {"username": "test1", "password": "asdf"}
     token = client.login(credentials=credentials_data)
-    assert token == "f4k3t0k3n"
-    assert client.session.headers["Authorization"] == "Token f4k3t0k3n"
+    assert token == "8cbecaf7b452d603a043ea41a8467661def00fca619f335d9a18535d02c36ea2"
+    assert (
+        client.session.headers["Authorization"]
+        == "Token 8cbecaf7b452d603a043ea41a8467661def00fca619f335d9a18535d02c36ea2"
+    )
 
     # test signout
     client.signout()
