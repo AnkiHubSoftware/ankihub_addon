cc7873f195864cee51d5227a7900ff97482419ca
Andrew Sanchez <inbox.asanchez@gmail.com>
2022-08-24 11:12:39 -0700

    Check token len.

--- tests/test_client.py
+++ tests/test_client.py
@@ -11,12 +11,5 @@ def test_client_login_and_signout(monkeypatch):
     client = AnkiHubClient()
     credentials_data = {"username": "test1", "password": "asdf"}
     token = client.login(credentials=credentials_data)
-    assert token == "8cbecaf7b452d603a043ea41a8467661def00fca619f335d9a18535d02c36ea2"
-    assert (
-        client.session.headers["Authorization"]
-        == "Token 8cbecaf7b452d603a043ea41a8467661def00fca619f335d9a18535d02c36ea2"
-    )
-
-    # test signout
-    client.signout()
-    assert client.session.headers["Authorization"] == ""
+    assert len(token) == 64
+    assert client.session.headers["Authorization"] == f"Token {token}"
