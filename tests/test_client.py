b7dbe14d5c934a282aea36129b33e90a40235686
RisingOrange <jakub.fidler@protonmail.com>
2022-08-27 18:40:14 +0200

    Test login and signout in one test

--- tests/test_client.py
+++ tests/test_client.py
@@ -4,7 +4,7 @@ pytestmark = pytest.mark.usefixtures("mw_mock")
 
 
 @pytest.mark.vcr()
-def test_client_login(monkeypatch):
+def test_client_login_and_signout(monkeypatch):
     monkeypatch.setenv("ANKIHUB_APP_URL", "http://localhost:8000")
     from ankihub.addon_ankihub_client import AddonAnkiHubClient as AnkiHubClient
 
@@ -13,3 +13,6 @@ def test_client_login(monkeypatch):
     token = client.login(credentials=credentials_data)
     assert len(token) == 64
     assert client.session.headers["Authorization"] == f"Token {token}"
+
+    client.signout()
+    assert client.session.headers["Authorization"] == ""
