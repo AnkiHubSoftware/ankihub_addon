bebbd908fe1802abedd08cf0b4a9aff5e9bfc864
Andrew Sanchez <inbox.asanchez@gmail.com>
2022-09-14 16:52:27 -0700

    Mocking.

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,7 +1,8 @@
+from unittest.mock import MagicMock
 import uuid
-from ankihub.ankihub_client import AnkiHubClient
 
 import pytest
+import requests_mock
 
 
 pytestmark = pytest.mark.usefixtures("mw_mock")
@@ -14,12 +15,16 @@ def set_ankihub_app_url(monkeypatch):
 
 @pytest.fixture
 def client():
+    from ankihub.ankihub_client import AnkiHubClient
+
     client = AnkiHubClient()
     yield client
 
 
 @pytest.fixture
 def authorized_client():
+    from ankihub.ankihub_client import AnkiHubClient
+
     client = AnkiHubClient()
     credentials_data = {"username": "test1", "password": "asdf"}
     client.login(credentials=credentials_data)
@@ -27,7 +32,7 @@ def authorized_client():
 
 
 @pytest.mark.vcr()
-def test_client_login_and_signout(client: AnkiHubClient):
+def test_client_login_and_signout(client):
     credentials_data = {"username": "test1", "password": "asdf"}
     token = client.login(credentials=credentials_data)
     assert len(token) == 64
@@ -38,7 +43,12 @@ def test_client_login_and_signout(client: AnkiHubClient):
 
 
 @pytest.mark.vcr()
-def test_download_deck(authorized_client: AnkiHubClient):
+def test_download_deck(authorized_client, monkeypatch):
     deck_id = uuid.UUID("dda0d3ad-89cd-45fb-8ddc-fabad93c2d7b")
-    notes_data = authorized_client.download_deck(ankihub_deck_uuid=deck_id)
+    get_presigned_url = MagicMock()
+    get_presigned_url.return_value = "https://fake_s3.com"
+    monkeypatch.setattr(authorized_client, "get_presigned_url", get_presigned_url)
+    with requests_mock.Mocker(real_http=True) as m:
+        m.register_uri("GET", get_presigned_url.return_value, content=b"a,b\n1,2")
+        notes_data = authorized_client.download_deck(ankihub_deck_uuid=deck_id)
     assert notes_data
