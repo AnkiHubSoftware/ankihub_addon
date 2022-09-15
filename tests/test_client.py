85ce55babd362d33ab1643e5cf8be613de521693
RisingOrange <jakub.fidler@protonmail.com>
2022-09-15 15:35:18 +0200

    Make test_download_deck work

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,9 +1,11 @@
-from unittest.mock import MagicMock
 import uuid
+from pathlib import Path
+from unittest.mock import MagicMock
 
 import pytest
 import requests_mock
 
+DECK_CSV_PATH = Path("tests/test_data/deck_with_one_basic_note.csv")
 
 pytestmark = pytest.mark.usefixtures("mw_mock")
 
@@ -44,11 +46,17 @@ def test_client_login_and_signout(client):
 
 @pytest.mark.vcr()
 def test_download_deck(authorized_client, monkeypatch):
+    from ankihub.ankihub_client import AnkiHubClient
+
+    client: AnkiHubClient = authorized_client
     deck_id = uuid.UUID("dda0d3ad-89cd-45fb-8ddc-fabad93c2d7b")
     get_presigned_url = MagicMock()
     get_presigned_url.return_value = "https://fake_s3.com"
-    monkeypatch.setattr(authorized_client, "get_presigned_url", get_presigned_url)
+    monkeypatch.setattr(client, "get_presigned_url", get_presigned_url)
     with requests_mock.Mocker(real_http=True) as m:
-        m.register_uri("GET", get_presigned_url.return_value, content=b"a,b\n1,2")
-        notes_data = authorized_client.download_deck(ankihub_deck_uuid=deck_id)
-    assert notes_data
+        m.register_uri(
+            "GET", get_presigned_url.return_value, content=DECK_CSV_PATH.read_bytes()
+        )
+        notes_data = client.download_deck(ankihub_deck_uuid=deck_id)
+    assert len(notes_data) == 1
+    assert notes_data[0].tags == ["asdf"]
