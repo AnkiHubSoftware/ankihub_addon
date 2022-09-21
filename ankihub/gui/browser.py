559ef6530d629c3c5961d6abde3990adf5f73cb4
RisingOrange <jakub.fidler@protonmail.com>
2022-09-21 23:35:02 +0200

    Remove bulk tag suggestions

--- ankihub/gui/browser.py
+++ ankihub/gui/browser.py
@@ -4,16 +4,13 @@ from typing import Optional
 from aqt import mw
 from aqt.browser import Browser
 from aqt.gui_hooks import browser_will_show_context_menu
-from aqt.operations import CollectionOp
 from aqt.qt import QMenu
-from aqt.utils import getTag, showInfo, showText, tooltip, tr
+from aqt.utils import showInfo, showText, tooltip, tr
 
 from .. import LOGGER
-from ..addon_ankihub_client import AddonAnkiHubClient as AnkiHubClient
 from ..ankihub_client import AnkiHubRequestError
 from ..settings import AnkiHubCommands
 from ..suggestions import suggest_notes_in_bulk
-from ..utils import ankihub_uuids_of_nids
 from .suggestion_dialog import SuggestionDialog
 
 
@@ -37,10 +34,6 @@ def on_browser_will_show_context_menu(browser: Browser, context_menu: QMenu) -> 
     menu.addAction(
         "AnkiHub: Bulk suggest notes",
         lambda: on_bulk_notes_suggest_action(browser),
-    )
-    menu.addAction(
-        "AnkiHub: Bulk suggest tags",
-        lambda: on_bulk_tag_suggestion_action(browser),
     )
 
 
@@ -98,45 +91,6 @@ def on_bulk_notes_suggest_action(browser: Browser) -> None:
         ),
         parent=browser,
     )
-
-
-def on_bulk_tag_suggestion_action(browser: Browser) -> None:
-    selected_nids = browser.selected_notes()
-    (tags, ok) = getTag(
-        parent=browser,
-        deck=mw.col,
-        question="Enter space-separated list of tags to add to selected notes.\n\n"
-        "* Tags will be added to notes that don't already have them.",
-    )
-
-    if not ok:
-        return
-
-    ankihub_note_uuids = ankihub_uuids_of_nids(selected_nids, ignore_invalid=True)
-
-    if not ankihub_note_uuids:
-        tooltip("No AnkiHub notes were selected.")
-        return
-
-    client = AnkiHubClient()
-    client.bulk_suggest_tags(ankihub_note_uuids, tags.split(" "))
-    LOGGER.debug("Bulk tag suggestion created.")
-    tooltip("Bulk tag suggestion created.", parent=browser)
-
-    # using this instead of browser.add_tags_to_notes to avoid tooltip conflict
-    # there will be no exception when adding the tags fails, this should not be a big problem
-    op = (
-        CollectionOp(browser, lambda col: col.tags.bulk_add(selected_nids, tags))
-        .success(
-            lambda out: LOGGER.debug(
-                "Added chosen tags to selected notes if they didn't have them yet."
-            )
-        )
-        .failure(
-            lambda exc: LOGGER.debug("Failed to add chosen tags to selected notes.")
-        )
-    )
-    op.run_in_background()
 
 
 def setup() -> None:
