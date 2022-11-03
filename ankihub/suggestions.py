202570901bbce19ccaf123de25fabb975a4f82d3
RisingOrange <jakub.fidler@protonmail.com>
2022-11-03 12:36:10 +0100

    exclude fields protected by tags from suggestions

--- ankihub/suggestions.py
+++ ankihub/suggestions.py
@@ -1,19 +1,12 @@
-from dataclasses import dataclass
-import re
 import uuid
+from dataclasses import dataclass
 from typing import Dict, List
 
 from anki.notes import Note, NoteId
 
 from .addon_ankihub_client import AddonAnkiHubClient as AnkiHubClient
-from .ankihub_client import (
-    ChangeNoteSuggestion,
-    Field,
-    NewNoteSuggestion,
-    SuggestionType,
-)
+from .ankihub_client import ChangeNoteSuggestion, NewNoteSuggestion, SuggestionType
 from .db import ankihub_db
-from .sync import is_internal_tag
 from .exporting import to_note_data
 from .utils import ankihub_uuid_of_note
 
@@ -105,15 +98,13 @@ def suggest_notes_in_bulk(
 def change_note_suggestion(
     note: Note, change_type: SuggestionType, comment: str
 ) -> ChangeNoteSuggestion:
-    ankihub_note_uuid = ankihub_uuid_of_note(note, ignore_invalid=False)
-    tags = _prepare_tags(note.tags)
-    fields = _prepare_fields(note)
+    note_data = to_note_data(note)
 
     return ChangeNoteSuggestion(
-        ankihub_note_uuid=ankihub_note_uuid,
+        ankihub_note_uuid=note_data.ankihub_note_uuid,
         anki_nid=note.id,
-        fields=fields,
-        tags=tags,
+        fields=note_data.fields,
+        tags=note_data.tags,
         change_type=change_type,
         comment=comment,
     )
@@ -122,47 +113,15 @@ def change_note_suggestion(
 def new_note_suggestion(
     note: Note, ankihub_deck_uuid: uuid.UUID, comment: str
 ) -> NewNoteSuggestion:
-    ankihub_note_uuid = ankihub_uuid_of_note(note, ignore_invalid=True)
-    if not ankihub_note_uuid:
-        ankihub_note_uuid = uuid.uuid4()
-
-    tags = _prepare_tags(note.tags)
-    fields = _prepare_fields(note)
+    note_data = to_note_data(note, set_new_id=True)
 
     return NewNoteSuggestion(
         ankihub_deck_uuid=ankihub_deck_uuid,
-        ankihub_note_uuid=ankihub_note_uuid,
+        ankihub_note_uuid=note_data.ankihub_note_uuid,
         anki_nid=note.id,
-        fields=fields,
-        tags=tags,
+        fields=note_data.fields,
+        tags=note_data.tags,
         note_type_name=note.note_type()["name"],
-        anki_note_type_id=note.note_type()["id"],
+        anki_note_type_id=note.mid,
         comment=comment,
     )
-
-
-def _prepare_fields(note: Note) -> List[Field]:
-
-    # Exclude the AnkiHub ID field since we don't want to expose this as an
-    # editable field in AnkiHub suggestion forms.
-    field_vals = list(note.fields[:-1])
-    fields_metadata = note.note_type()["flds"][:-1]
-
-    prepared_field_vals = [_prepared_field_html(field) for field in field_vals]
-    fields = [
-        Field(name=field["name"], order=field["ord"], value=val)
-        for field, val in zip(fields_metadata, prepared_field_vals)
-    ]
-    return fields
-
-
-def _prepared_field_html(html: str) -> str:
-    # Since Anki 2.1.54 data-editor-shrink attribute="True" is added to img tags when you double click them.
-    # We don't want this attribute to appear in suggestions.
-    result = re.sub(r" ?data-editor-shrink=['\"]true['\"]", "", html)
-    return result
-
-
-def _prepare_tags(tags: List[str]):
-    # removing empty tags is necessary because notes have empty tags in the editor sometimes
-    return [tag for tag in tags if tag.strip() and not is_internal_tag(tag)]
