725973be30bb931d3559a9e2415ba94f2d36ec21
RisingOrange <31575114+RisingOrange@users.noreply.github.com>
2022-11-28 22:10:16 +0100

    perf: Decompress deck csvs (#355)
    * Look for error details in "detail" field too
    
    * Decompress deck CSVs if they have a .gz file extension
    
    * Fix tests and add test

--- ankihub/errors.py
+++ ankihub/errors.py
@@ -38,7 +38,7 @@ def handle_exception(
 
         try:
             response_data = exc.response.json()
-            details = response_data["details"]
+            details = response_data.get("detail") or response_data.get("details")
         except:
             details = None
 
