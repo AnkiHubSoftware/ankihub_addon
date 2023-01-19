c91612a3c695b0757ab0dfcd60381bf7b4c24c48
RisingOrange <jakub.fidler@protonmail.com>
2023-01-19 20:03:01 +0100

    ref: refactor client test

--- tests/client/test_client.py
+++ tests/client/test_client.py
@@ -610,7 +610,10 @@ def test_get_note_customizations_by_deck_extension_id(authorized_client_for_user
         ],
     }
 
-    for chunk in client.get_note_customizations_by_deck_extension_id(
-        deck_extension_id=deck_extension_id
-    ):
-        assert chunk == expected_response
+    chunks = list(
+        client.get_note_customizations_by_deck_extension_id(
+            deck_extension_id=deck_extension_id
+        )
+    )
+    assert len(chunks) == 1
+    assert chunks[0] == expected_response
