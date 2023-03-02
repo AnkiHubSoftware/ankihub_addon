565bac3456c0d10adf89f6be52944b792a9cab14
RisingOrange <jakub.fidler@protonmail.com>
2023-03-02 22:51:58 +0100

    add docstring

--- ankihub/media_utils.py
+++ ankihub/media_utils.py
@@ -7,6 +7,9 @@ from anki.utils import ids2str
 
 
 def get_img_names_from_notes(nids: Sequence[NoteId]) -> Set[str]:
+    """Return the names of all images on the given notes.
+    Does only return names of local images, not remote images."""
+
     flds_with_imgs = aqt.mw.col.db.list(
         f"SELECT flds FROM notes WHERE id IN {ids2str(nids)} AND flds LIKE '%<img%'",
     )
