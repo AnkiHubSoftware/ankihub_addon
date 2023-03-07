98affa8e42c7fa7202973630ed89d561bb9ba6c4
RisingOrange <jakub.fidler@protonmail.com>
2023-03-07 16:32:15 +0100

    Revert "ignore type errors in mashumaro"
    This reverts commit c5739e25fdc8b34d5bd45f3fc8269bc006fa932c.

--- ankihub/lib/mashumaro/core/meta/patch.py
+++ ankihub/lib/mashumaro/core/meta/patch.py
@@ -5,7 +5,7 @@ def patch_fromisoformat():
     if PY_36:
         # noinspection PyPackageRequirements
         # noinspection PyUnresolvedReferences
-        from backports.datetime_fromisoformat import MonkeyPatch # type: ignore
+        from backports.datetime_fromisoformat import MonkeyPatch
 
         MonkeyPatch.patch_fromisoformat()
 
