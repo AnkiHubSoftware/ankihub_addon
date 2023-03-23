a7231a8f9d3295b38a57b7746e0f60ff957d7b31
RisingOrange <jakub.fidler@protonmail.com>
2023-03-24 00:15:25 +0100

    refactor

--- ankihub/debug.py
+++ ankihub/debug.py
@@ -2,7 +2,7 @@
 import os
 import traceback
 from pathlib import Path
-from typing import Any, Callable
+from typing import Any, Callable, Dict
 
 import aqt
 from anki.hooks import wrap
@@ -26,25 +26,28 @@ def _with_sentry_report_about_user_files_on_error(*args: Any, **kwargs: Any) -> 
 
 
 def _report_user_files_debug_info_to_sentry(e: Exception) -> None:
+    report_exception_and_upload_logs(e, context=_user_files_context_dict())
+
+
+def _user_files_context_dict() -> Dict[str, Dict[str, Any]]:
+    """Return a dict with information about the user files of the AnkiHub add-on."""
     ankihub_module = aqt.mw.addonManager.addonFromModule(__name__)
     user_files_path = Path(aqt.mw.addonManager._userFilesPath(ankihub_module))
     all_file_paths = [user_files_path, *list(user_files_path.rglob("*"))]
-    problematic_files = []
-    for file in all_file_paths:
-        if is_win and not _file_is_accessible(file):
-            problematic_files.append(file)
+    problematic_file_paths = []
+    if is_win:
+        problematic_file_paths = [
+            file for file in all_file_paths if not _file_is_accessible(file)
+        ]
 
-    problematic_file_paths = [str(file) for file in problematic_files]
-    report_exception_and_upload_logs(
-        e,
-        context={
-            "Add-on update debug info": {
-                "all files": [str(file) for file in all_file_paths],
-                "inaccessible files": problematic_file_paths,
-                "Is ankihub database attached to anki database?": is_ankihub_db_attached_to_anki_db(),
-            },
+    result = {
+        "Add-on update debug info": {
+            "all files": [str(file) for file in all_file_paths],
+            "inaccessible files": [str(file) for file in problematic_file_paths],
+            "Is ankihub database attached to anki database?": is_ankihub_db_attached_to_anki_db(),
         },
-    )
+    }
+    return result
 
 
 def _file_is_accessible(f: Path) -> bool:
@@ -65,8 +68,7 @@ def _log_stack(title: str):
     LOGGER.info(f"Stack trace ({title}):\n{stack_trace}")
 
 
-def setup():
-
+def _setup_logging_for_sync_collection_and_media():
     # Log stack trace when mw._sync_collection_and_media is called to debug the
     # "Cannot start transaction within transaction" error that occurs when two syncs
     # are started at the same time.
@@ -76,7 +78,9 @@ def setup():
         "before",
     )
 
-    # Log additional information when an add-on update fails.
+
+def _setup_sentry_reporting_for_error_on_addon_update():
+    # Sends additional information to Sentry when an add-on update fails.
     # When Anki tries to backup user files before updating an add-on, it fails sometimes with
     # an PermissionError on windows. We are already doing things to prevent this: we are closing the log file handler,
     # changing permissions of files in the folder and detaching the ankihub database from the Anki database.
@@ -87,3 +91,8 @@ def setup():
         new=_with_sentry_report_about_user_files_on_error,
         pos="around",
     )
+
+
+def setup():
+    _setup_logging_for_sync_collection_and_media()
+    _setup_sentry_reporting_for_error_on_addon_update()
