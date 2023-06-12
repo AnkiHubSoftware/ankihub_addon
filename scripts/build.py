6c5c272b8d20a0840d8d52132c35529b7ad9bcec
Pedro Costa <pedrobeethoven8@gmail.com>
2023-06-12 15:02:03 -0300

    fix: Fix version issues (#639)

--- scripts/build.py
+++ scripts/build.py
@@ -3,6 +3,8 @@ import shutil
 import subprocess
 from pathlib import Path
 
+import _aqt
+import anki
 from generate_manifest import generate_manifest
 from google_api_obfuscate import obfuscate_google_api_key
 
@@ -44,3 +46,14 @@ shutil.rmtree(MEDIA_EXPORT_TARGET, ignore_errors=True)
 shutil.copytree(MEDIA_EXPORT_SRC, MEDIA_EXPORT_TARGET)
 
 obfuscate_google_api_key(GOOGLE_API_KEY, MEDIA_IMPORT_TARGET)
+
+
+def create_init_file(module):
+    module_path = Path(module.__path__[0])
+    init_file_path = module_path / "__init__.py"
+    init_file_path.touch(exist_ok=True)
+
+
+# This makes mypy type checking work on Anki versions >= 2.1.55
+create_init_file(anki)
+create_init_file(_aqt)
