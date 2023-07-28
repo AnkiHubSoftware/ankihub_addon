f77f11ac152eace097fc4e748d0997749d9b0d50
Pedro <pedrobeethoven8@gmail.com>
2023-07-28 15:57:08 -0300

    Add new module use_cases

---
+++ ankihub/use_cases.py
@@ -0,0 +1,10 @@
+from ankihub.utils import undo_note_type_modfications
+from .db import ankihub_db
+
+
+def unsubscribe_from_deck(client, deck_ankihub_id, config):
+    client.unsubscribe_from_deck(deck_ankihub_id)
+    config.remove_deck(deck_ankihub_id)
+    mids = ankihub_db.note_types_for_ankihub_deck(deck_ankihub_id)
+    undo_note_type_modfications(mids)
+    ankihub_db.remove_deck(deck_ankihub_id)
