65abbd8d190ed962c1b3ca5ab4ef01030a5dc393
RisingOrange <jakub.fidler@protonmail.com>
2023-08-21 16:36:26 +0200

    Add test for exporting

--- tests/addon/performance/test_exporting.py
+++ tests/addon/performance/test_exporting.py
@@ -1,13 +1,55 @@
-# import os
+import os
+import uuid
+from typing import Callable, Dict, List
 
-# from anki.models import NotetypeDict, NotetypeId
-# from pytest_anki import AnkiSession
+from anki.models import NotetypeDict, NotetypeId
+from pytest_anki import AnkiSession
 
-# from ankihub.ankihub_client import NoteInfo
-# from ankihub.ankihub_client.ankihub_client import CSV_DELIMITER, _transform_notes_data
+from ankihub.ankihub_client import NoteInfo
 
-# # workaround for vscode test discovery not using pytest.ini which sets this env var
-# # has to be set before importing ankihub
-# os.environ["SKIP_INIT"] = "1"
+from .conftest import ProfileAndTime
 
-# from ankihub.main.importing import AnkiHubImporter
+# workaround for vscode test discovery not using pytest.ini which sets this env var
+# has to be set before importing ankihub
+os.environ["SKIP_INIT"] = "1"
+
+from ankihub.main.exporting import to_note_data
+from ankihub.main.importing import AnkiHubImporter
+
+
+def test_anking_export_without_changes(
+    anki_session_with_addon_data: AnkiSession,
+    next_deterministic_uuid: Callable[[], uuid.UUID],
+    anking_notes_data: List[NoteInfo],
+    anking_note_types: Dict[NotetypeId, NotetypeDict],
+    profile_and_time: ProfileAndTime,
+):
+    """Test that exporting a portion of the AnKing deck takes less than a threshold duration."""
+    with anki_session_with_addon_data.profile_loaded():
+        mw = anki_session_with_addon_data.mw
+
+        notes_amount = 1000
+
+        # Import notes
+        notes_data = anking_notes_data[:notes_amount]
+        importer = AnkiHubImporter()
+        importer.import_ankihub_deck(
+            ankihub_did=next_deterministic_uuid(),
+            notes=notes_data,
+            deck_name="test",
+            is_first_import_of_deck=True,
+            note_types=anking_note_types,
+            protected_fields={},
+            protected_tags=[],
+        )
+
+        # Assert that exporting the notes takes less than 0.3 seconds
+        notes = [mw.col.get_note(nid) for nid in mw.col.find_notes("")]
+        assert len(notes) == notes_amount  # sanity check
+
+        def export_notes():
+            for note in notes:
+                to_note_data(note)
+
+        duration = profile_and_time(export_notes)
+        assert duration < 0.3
