09e0d70dd7b7f518674abb8a9e4c75522d68e721
RisingOrange <jakub.fidler@protonmail.com>
2023-08-21 14:53:53 +0200

    Begin adding test for export

---
+++ tests/addon/performance/test_exporting.py
@@ -0,0 +1,13 @@
+# import os
+
+# from anki.models import NotetypeDict, NotetypeId
+# from pytest_anki import AnkiSession
+
+# from ankihub.ankihub_client import NoteInfo
+# from ankihub.ankihub_client.ankihub_client import CSV_DELIMITER, _transform_notes_data
+
+# # workaround for vscode test discovery not using pytest.ini which sets this env var
+# # has to be set before importing ankihub
+# os.environ["SKIP_INIT"] = "1"
+
+# from ankihub.main.importing import AnkiHubImporter
