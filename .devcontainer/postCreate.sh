f56d05fb2c34dadc605a51d6e34942e333c28ff3
Andrew Sanchez <inbox.asanchez@gmail.com>
2023-09-22 14:55:47 -0700

    Correct paths.

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -2,7 +2,7 @@
 
 # link workspace in anki addons
 mkdir -p $HOME/.local/share/Anki2/addons21
-ln -s /workspaces/anki-hyper-tts $HOME/.local/share/Anki2/addons21/anki-hyper-tts
+ln -s /workspaces/ankihub $HOME/.local/share/Anki2/addons21/ankihub
 
 # VNC setup
 sudo ln -s $PWD/.devcontainer/config/novnc/index.html /opt/novnc/index.html
