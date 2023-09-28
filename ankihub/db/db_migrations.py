c9a320dbaeb422fecfc4c355094394ef717e2d5a
Jakub Fidler <31575114+RisingOrange@users.noreply.github.com>
2023-09-28 18:13:07 +0200

    fix: `temp_notetypes` table already exists - failed db migration (#760)
    * fix: Wrap migration into transaction
    
    * fix: Remove temp_notetypes table on migration start
    
    * chore: Move log statements

--- ankihub/db/db_migrations.py
+++ ankihub/db/db_migrations.py
@@ -126,18 +126,22 @@ def migrate_ankihub_db():
     if ankihub_db.schema_version() < 10:
         # Migrate note_types to new table which has a different primary key
         # To do that in sqlite, we need to create a new table, copy the data over and then delete the old table
-        ankihub_db.execute("ALTER TABLE notetypes RENAME TO temp_notetypes;")
-        ankihub_db._setup_note_types_table()
-        ankihub_db.execute(
-            """
-            INSERT INTO notetypes (anki_note_type_id, ankihub_deck_id, name, note_type_dict_json)
-            SELECT anki_note_type_id, ankihub_deck_id, name, note_type_dict_json
-            FROM temp_notetypes;
-            """
-        )
-        ankihub_db.execute("DROP TABLE temp_notetypes;")
+        with ankihub_db.connection() as conn:
+            # Previously the migration was not wrapped in a transaction and the temp_note_types table was not dropped,
+            # so we need to drop it here if it exists
+            conn.execute("DROP TABLE IF EXISTS temp_notetypes;")
+            conn.execute("ALTER TABLE notetypes RENAME TO temp_notetypes;")
+            ankihub_db._setup_note_types_table(conn)
+            conn.execute(
+                """
+                INSERT INTO notetypes (anki_note_type_id, ankihub_deck_id, name, note_type_dict_json)
+                SELECT anki_note_type_id, ankihub_deck_id, name, note_type_dict_json
+                FROM temp_notetypes;
+                """
+            )
+            conn.execute("DROP TABLE temp_notetypes;")
 
-        ankihub_db.execute("PRAGMA user_version = 10;")
+            conn.execute("PRAGMA user_version = 10;")
 
         LOGGER.info(
             f"AnkiHub DB migrated to schema version {ankihub_db.schema_version()}"
