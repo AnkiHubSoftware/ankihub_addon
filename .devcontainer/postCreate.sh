8104f84c0e41133fc881d9c0c75ddf513aa97572
Abdo <abdo@abdnh.net>
2023-10-03 20:27:57 +0000

    Use Python venv

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -2,8 +2,14 @@
 
 # link workspace in anki addons
 mkdir -p $HOME/.local/share/Anki2/addons21
-ln -s /workspaces/ankihub $HOME/.local/share/Anki2/addons21/ankihub
+ln -s /workspaces/ankihub_addon/ankihub $HOME/.local/share/Anki2/addons21/ankihub
 
 # VNC setup
 sudo ln -s $PWD/.devcontainer/config/novnc/index.html /opt/novnc/index.html
 cp -rv $PWD/.devcontainer/config/.vnc $HOME/
+
+# Install Python dependencies
+VENV_PATH="/home/vscode/venv"
+python -m venv ${VENV_PATH}
+${VENV_PATH}/bin/pip install --upgrade pip
+${VENV_PATH}/bin/pip --no-cache-dir install -r /workspaces/ankihub_addon/requirements/dev.txt
