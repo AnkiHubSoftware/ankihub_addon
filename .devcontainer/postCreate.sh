e5333ea2e224a838e13e6148ce1aa9bff326e976
RisingOrange <jakub.fidler@protonmail.com>
2023-10-05 00:38:47 +0200

    Set up venv in Dockerfile

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -7,9 +7,3 @@ ln -s /workspaces/ankihub_addon/ankihub $HOME/.local/share/Anki2/addons21/ankihu
 # VNC setup
 sudo ln -s $PWD/.devcontainer/config/novnc/index.html /opt/novnc/index.html
 cp -rv $PWD/.devcontainer/config/.vnc $HOME/
-
-# Install Python dependencies
-VENV_PATH="/home/vscode/venv"
-python -m venv ${VENV_PATH}
-${VENV_PATH}/bin/pip install --upgrade pip
-${VENV_PATH}/bin/pip --no-cache-dir install -r /workspaces/ankihub_addon/requirements/dev.txt
