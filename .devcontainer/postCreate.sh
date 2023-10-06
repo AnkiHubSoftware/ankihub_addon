b14fc269ad25648abd7d4165be9b4fde15e0f0fa
Andrew Sanchez <inbox.asanchez@gmail.com>
2023-10-06 06:32:04 -0700

    Set up VSCode devcontainer (#754)
    * initial commit for devcontainer.
    
    * install deps.
    
    * Formatting.
    
    * Correct paths.
    
    * Use python image.
    
    * Update system deps.
    
    * Remove missing package
    
    * Update dockerfile.
    
    * requirements.
    
    * tweak dependencies.
    
    * Remove gitpod.
    
    * Use Python venv
    
    * Add xvfb and tigervnc to dependencies
    
    * Use qt5 version of Anki
    
    * Add more dependencies
    
    * Formatting
    
    * Set up venv in Dockerfile
    
    * Fix blank screen in Anki
    
    * Modify path in .profile
    
    * Run build script in postStart.sh
    
    * Set env vars for staging
    
    * Disable error reporting in add-on
    
    * Set interpreter path in devcontainer.json correctly
    
    * Make vcr client tests work
    
    * Ignore mypy error for anki._vendor
    
    ---------
    
    Co-authored-by: Abdo <abdo@abdnh.net>
    Co-authored-by: RisingOrange <jakub.fidler@protonmail.com>

---
+++ .devcontainer/postCreate.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# link workspace in anki addons
+mkdir -p $HOME/.local/share/Anki2/addons21
+ln -s /workspaces/ankihub_addon/ankihub $HOME/.local/share/Anki2/addons21/ankihub
+
+# VNC setup
+sudo ln -s $PWD/.devcontainer/config/novnc/index.html /opt/novnc/index.html
+cp -rv $PWD/.devcontainer/config/.vnc $HOME/
