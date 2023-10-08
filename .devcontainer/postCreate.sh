6d615c4af4df071220c7f356b1cadc4c6ac03823
RisingOrange <jakub.fidler@protonmail.com>
2023-10-08 14:39:50 +0200

    Run build script in postStart.sh

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -10,3 +10,6 @@ cp -rv $PWD/.devcontainer/config/.vnc $HOME/
 
 # Pre-commit setup
 pre-commit install
+
+# TODO: Set api key properly
+sudo GOOGLE_API_KEY="fake_google_api_key" $HOME/venv/bin/python scripts/build.py
