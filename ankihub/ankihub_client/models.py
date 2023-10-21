1f0c0b37560f1586c8a0a577b55e403e0c0893a8
RisingOrange <jakub.fidler@protonmail.com>
2023-10-21 13:49:48 +0200

    Add `CardReviewData` model to client

--- ankihub/ankihub_client/models.py
+++ ankihub/ankihub_client/models.py
@@ -280,6 +280,19 @@ class DeckExtensionUpdateChunk(DataClassJSONMixinWithConfig):
     )
 
 
+@dataclass
+class CardReviewData(DataClassJSONMixinWithConfig):
+    ah_did: uuid.UUID = dataclasses.field(metadata=field_options(alias="deck_id"))
+    total_card_reviews_last_30_days: int
+    last_card_review_at: Optional[datetime] = dataclasses.field(
+        metadata=field_options(
+            deserialize=lambda x: datetime.strptime(x, ANKIHUB_DATETIME_FORMAT_STR)
+            if x
+            else None,
+        ),
+    )
+
+
 # Media related functions
 
 
