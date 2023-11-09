ad46745b1a96bf5da104bcdd6d2303540d38b157
Jakub Fidler <31575114+RisingOrange@users.noreply.github.com>
2023-11-09 15:19:19 +0100

    perf: Improve performance of importing AnkiHub deck (#794)
    * fix: Use `anki.utils.point_version`
    
    * Fix sorting args of browser column
    
    * Update anki version in requirements
    
    * Don't call db.begin() on Anki >= 23.10
    
    * Don't monkey patch `db.begin` on 23.10
    
    * Add PyQt5 requirements
    
    * Add "type:  ignore" comments
    
    * Fix test
    
    * Refactor
    
    * Merge
    
    * Make card suspension work again, use `col.update_cards`
    
    * ref: Remove unnecessary function
    
    * Refactor
    
    * ref: `_import_notes`
    
    * Refactor `_import_notes` some more
    
    * ref: `_create_notes_inner`
    
    * Add `col.save()` back in (it's a no-op on 23.10)
    
    * Add docstring and type hint
    
    * Add performance test for deck update
    
    * Make it work on Anki 2.1.56 by using `col.add_note`
    
    * Edit comment
    
    * ref: Rename `duration` to `duration_seconds`
    
    * Fix first_time_import test
    
    * Add *.pstats to gitignore
    
    * Remove newline in test
    
    * ref: Remove duplicate call to set suspend option

--- tests/addon/performance/test_ankihub_importer.py
+++ tests/addon/performance/test_ankihub_importer.py
@@ -1,6 +1,5 @@
 import os
-import uuid
-from typing import Callable, Dict, List
+from typing import Dict, List, Protocol
 
 import pytest
 from anki.models import NotetypeDict, NotetypeId
@@ -18,30 +17,71 @@ os.environ["SKIP_INIT"] = "1"
 from ankihub.main.importing import AnkiHubImporter
 
 
+class ImportAnkingNotes(Protocol):
+    def __call__(
+        self,
+        anking_notes_data: List[NoteInfo],
+    ) -> None:
+        ...
+
+
+@pytest.fixture
+def import_anking_notes(
+    next_deterministic_uuid,
+    anking_note_types: Dict[NotetypeId, NotetypeDict],
+) -> ImportAnkingNotes:
+    """Imports the given AnKing NoteInfos using the AnkHubImporter."""
+
+    def import_anking_notes_inner(
+        anking_notes_data: List[NoteInfo],
+    ) -> None:
+        importer = AnkiHubImporter()
+        importer.import_ankihub_deck(
+            ankihub_did=next_deterministic_uuid(),
+            notes=anking_notes_data,
+            deck_name="test",
+            is_first_import_of_deck=True,
+            note_types=anking_note_types,
+            protected_fields={},
+            protected_tags=[],
+            suspend_new_cards_of_new_notes=False,
+            suspend_new_cards_of_existing_notes=DeckConfig.suspend_new_cards_of_existing_notes_default(),
+        )
+
+    return import_anking_notes_inner
+
+
 @pytest.mark.performance
 def test_anking_deck_first_time_import(
     anki_session_with_addon_data: AnkiSession,
-    next_deterministic_uuid: Callable[[], uuid.UUID],
     anking_notes_data: List[NoteInfo],
-    anking_note_types: Dict[NotetypeId, NotetypeDict],
+    import_anking_notes: ImportAnkingNotes,
     profile: Profile,
 ):
-    """Test that importing a portion of the AnKing deck takes less than a threshold duration."""
+    """Test that importing a portion of the AnKing deck for the first time takes less than a threshold duration."""
     with anki_session_with_addon_data.profile_loaded():
         notes_data = anking_notes_data[:100]
-        importer = AnkiHubImporter()
-        duration = profile(
-            lambda: importer.import_ankihub_deck(
-                ankihub_did=next_deterministic_uuid(),
-                notes=notes_data,
-                deck_name="test",
-                is_first_import_of_deck=True,
-                note_types=anking_note_types,
-                protected_fields={},
-                protected_tags=[],
-                suspend_new_cards_of_new_notes=False,
-                suspend_new_cards_of_existing_notes=DeckConfig.suspend_new_cards_of_existing_notes_default(),
-            )
-        )
-        print(f"Importing {len(notes_data)} notes took {duration} seconds")
-        assert duration < 0.5
+        duration_seconds = profile(lambda: import_anking_notes(notes_data))
+        print(f"Importing {len(notes_data)} notes took {duration_seconds} seconds")
+        assert duration_seconds < 0.5
+
+
+@pytest.mark.performance
+def test_anking_deck_update(
+    anki_session_with_addon_data: AnkiSession,
+    anking_notes_data: List[NoteInfo],
+    import_anking_notes: ImportAnkingNotes,
+    profile: Profile,
+):
+    """Test that updating a portion of the AnKing deck takes less than a threshold duration."""
+    with anki_session_with_addon_data.profile_loaded():
+        notes_data = anking_notes_data[:100]
+        import_anking_notes(notes_data)
+
+        for note in notes_data:
+            note.fields[0].value = "updated"
+            note.tags = ["updated"]
+
+        duration_seconds = profile(lambda: import_anking_notes(notes_data))
+        print(f"Importing {len(notes_data)} notes took {duration_seconds} seconds")
+        assert duration_seconds < 0.5
