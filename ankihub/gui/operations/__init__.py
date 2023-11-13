cd3ad1de28e8308fcc45c996fdb42ee7cb5b39c2
Jakub Fidler <31575114+RisingOrange@users.noreply.github.com>
2023-11-13 18:36:46 +0100

    ref: Introduce `ANKI_VERSION_23_10_00` constant (#806)
    Intrdouce `ANKI_VERSION_23_10_10` constant

--- ankihub/gui/operations/__init__.py
+++ ankihub/gui/operations/__init__.py
@@ -1,6 +1,6 @@
 from aqt.operations import QueryOp
 
-from ...settings import ANKI_MINOR
+from ...settings import ANKI_MINOR, ANKI_VERSION_23_10_00
 
 
 class AddonQueryOp(QueryOp):
@@ -21,7 +21,7 @@ class AddonQueryOp(QueryOp):
         already running in parallel, so there is no need to do anything.
         This way we can use the same code for all Anki versions.
         """
-        if ANKI_MINOR < 231000:
+        if ANKI_MINOR < ANKI_VERSION_23_10_00:
             return self
 
         return super().without_collection()
