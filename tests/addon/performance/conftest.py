1279bd31dd177796abe6c27eb8a01c1095076822
RisingOrange <jakub.fidler@protonmail.com>
2023-11-17 15:38:48 +0100

    Tweak profiling results file name

--- tests/addon/performance/conftest.py
+++ tests/addon/performance/conftest.py
@@ -57,12 +57,17 @@ class WriteProfilingStats(Protocol):
 
 
 @pytest.fixture
-def write_profiling_stats(current_test_name) -> WriteProfilingStats:
+def write_profiling_stats(
+    current_test_parent_node_name: str, current_test_name: str
+) -> WriteProfilingStats:
     """Write the profiling stats to a file in the profiling stats directory.
-    The file is named after the current test name."""
+    The file is named after the current test name and its parent node."""
 
     def _write_profiling_stats(profiler: cProfile.Profile) -> None:
-        stats_path = PROFILING_STATS_DIR / f"{current_test_name}.pstats"
+        stats_path = (
+            PROFILING_STATS_DIR
+            / f"{current_test_parent_node_name.split('.')[0]}_{current_test_name}.pstats"
+        )
         profiler.dump_stats(stats_path)
 
     return _write_profiling_stats
@@ -71,6 +76,11 @@ def write_profiling_stats(current_test_name) -> WriteProfilingStats:
 @pytest.fixture
 def current_test_name(request) -> str:
     return request.node.name
+
+
+@pytest.fixture
+def current_test_parent_node_name(request) -> str:
+    return request.node.parent.name
 
 
 class Profile(Protocol):
