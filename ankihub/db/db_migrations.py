0069ebf22d8589579dd5a75ab7161be25076610b
RisingOrange <jakub.fidler@protonmail.com>
2024-01-26 13:07:16 +0100

    ref: Remove unreachable code

--- ankihub/db/db_migrations.py
+++ ankihub/db/db_migrations.py
@@ -16,18 +16,6 @@ def migrate_ankihub_db():
 
     LOGGER.info(f"AnkiHub DB schema version: {ankihub_db.schema_version()}")
 
-    if ankihub_db.schema_version() < 1:
-        with ankihub_db.connection() as conn:
-            conn.execute(
-                """
-                ALTER TABLE notes ADD COLUMN mod INTEGER
-                """
-            )
-            conn.execute("PRAGMA user_version = 1;")
-        LOGGER.info(
-            f"AnkiHub DB migrated to schema version {ankihub_db.schema_version()}"
-        )
-
     if ankihub_db.schema_version() < 2:
         with ankihub_db.connection() as conn:
             conn.execute("CREATE INDEX ankihub_deck_id_idx ON notes (ankihub_deck_id)")
