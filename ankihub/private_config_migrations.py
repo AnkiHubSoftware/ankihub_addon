fcca707509af514e460fd13917d013ae0e3fe8b2
RisingOrange <jakub.fidler@protonmail.com>
2024-02-15 16:31:37 +0100

    Add private config migration that prompts users for the behavior on delete option

--- ankihub/private_config_migrations.py
+++ ankihub/private_config_migrations.py
@@ -1,6 +1,22 @@
-from typing import Dict
+import uuid
+from typing import TYPE_CHECKING, Dict, List, Tuple
+
+import aqt
+from aqt.qt import (
+    QCheckBox,
+    QDialog,
+    QDialogButtonBox,
+    QGridLayout,
+    QHBoxLayout,
+    QLabel,
+    QVBoxLayout,
+    qconnect,
+)
 
 from . import LOGGER
+
+if TYPE_CHECKING:
+    from .settings import BehaviorOnRemoteNoteDeleted
 
 
 def migrate_private_config(private_config_dict: Dict) -> None:
@@ -25,6 +41,7 @@ def migrate_private_config(private_config_dict: Dict) -> None:
         private_config_dict
     )
     _remove_orphaned_deck_extensions(private_config_dict)
+    _maybe_prompt_user_for_behavior_on_remote_note_deleted(private_config_dict)
 
 
 def _maybe_reset_media_update_timestamps(private_config_dict: Dict) -> None:
@@ -92,3 +109,122 @@ def _remove_orphaned_deck_extensions(private_config_dict: Dict) -> None:
                 f"Removed deck extension config with {deck_extension_id=} for deck {deck_extension['ah_did']} "
                 "because the deck isn't in the config anymore."
             )
+
+
+def _maybe_prompt_user_for_behavior_on_remote_note_deleted(
+    private_config_dict: Dict,
+) -> None:
+    """Prompt the user to configure the behavior on remote note deleted for each deck if it's not set yet."""
+    field_name = "behavior_on_remote_note_deleted"
+
+    decks_dict = private_config_dict["decks"]
+    if all(deck.get(field_name) is not None for deck in decks_dict.values()):
+        return
+
+    LOGGER.info("Prompting user to configure behavior on remote note deleted.")
+
+    deck_id_and_name_tuples = [
+        (uuid.UUID(ah_did_str), deck["name"]) for ah_did_str, deck in decks_dict.items()
+    ]
+    dialog = ConfigureDeletedNotesDialog(
+        parent=aqt.mw, deck_id_and_name_tuples=deck_id_and_name_tuples
+    )
+    dialog.exec()
+
+    for (
+        deck_id,
+        behavior_on_remote_note_deleted,
+    ) in dialog.deck_id_to_behavior_on_remote_note_deleted_dict().items():
+        decks_dict[str(deck_id)][field_name] = behavior_on_remote_note_deleted.value
+
+    LOGGER.info("Configured behavior on remote note deleted.")
+
+
+class ConfigureDeletedNotesDialog(QDialog):
+    """Dialog to configure the behavior when a remote note is deleted for each deck.
+    Shows a list of decks and a checkbox for each deck to configure the behavior.
+    """
+
+    def __init__(
+        self, parent, deck_id_and_name_tuples: List[Tuple[uuid.UUID, str]]
+    ) -> None:
+        super().__init__(parent)
+
+        self._deck_id_and_name_tuples = deck_id_and_name_tuples
+        self._setup_ui()
+
+    def deck_id_to_behavior_on_remote_note_deleted_dict(
+        self,
+    ) -> Dict[uuid.UUID, "BehaviorOnRemoteNoteDeleted"]:
+        from .settings import BehaviorOnRemoteNoteDeleted
+
+        result: Dict[uuid.UUID, BehaviorOnRemoteNoteDeleted] = {}
+        for i, (deck_id, _) in enumerate(self._deck_id_and_name_tuples, 1):
+            checkbox_layout = self.grid_layout.itemAtPosition(i, 1).layout()
+            delete_checkbox: QCheckBox = checkbox_layout.itemAt(1).widget()
+            result[deck_id] = (
+                BehaviorOnRemoteNoteDeleted.DELETE_IF_NO_REVIEWS
+                if delete_checkbox.isChecked()
+                else BehaviorOnRemoteNoteDeleted.NEVER_DELETE
+            )
+        return result
+
+    def _setup_ui(self) -> None:
+        self.setWindowTitle("Configure deleted notes")
+
+        self.top_label = QLabel(
+            "When AnkiHub deletes notes that I have no review history with, they<br>"
+            "should also be removed locally from these decks..."
+        )
+
+        self.grid_layout = self._setup_grid_layout()
+
+        self.bottom_label = QLabel(
+            "You can adjust this setting later in the <b>Deck Management</b> menu."
+        )
+
+        self.button_box = QDialogButtonBox(QDialogButtonBox.StandardButton.Ok)
+        qconnect(self.button_box.accepted, self.accept)
+
+        self.main_layout = QVBoxLayout()
+        self.main_layout.setContentsMargins(20, 20, 20, 20)
+
+        self.main_layout.addWidget(self.top_label)
+        self.main_layout.addSpacing(10)
+        self.main_layout.addLayout(self.grid_layout)
+        self.main_layout.addSpacing(10)
+        self.main_layout.addWidget(self.bottom_label)
+        self.main_layout.addSpacing(25)
+        self.main_layout.addWidget(self.button_box)
+
+        self.setLayout(self.main_layout)
+
+    def _setup_grid_layout(self) -> QGridLayout:
+        self.grid_layout = QGridLayout()
+
+        # Add headers to the grid layout
+        self.deck_name_label = QLabel("<b>Deck name:</b>")
+        self.delete_label = QLabel("<b>Delete:</b>")
+
+        self.grid_layout.addWidget(self.deck_name_label, 0, 0)
+        self.grid_layout.addWidget(self.delete_label, 0, 1)
+
+        # Setup a row for each deck
+        for i, (_, deck_name) in enumerate(self._deck_id_and_name_tuples):
+            deck_label = QLabel(deck_name)
+            deck_label.setWordWrap(True)
+
+            deck_checkbox = QCheckBox()
+            checkbox_layout = QHBoxLayout()
+            checkbox_layout.addSpacing(20)
+            checkbox_layout.addWidget(deck_checkbox)
+
+            # Add deck label and checkbox to the row
+            # Offset by 1 due to header row.
+            self.grid_layout.addWidget(deck_label, i + 1, 0)
+            self.grid_layout.addLayout(checkbox_layout, i + 1, 1)
+            self.grid_layout.setRowMinimumHeight(i + 1, 40)
+
+        self.grid_layout.setColumnStretch(0, 3)
+        self.grid_layout.setColumnStretch(1, 1)
+        return self.grid_layout
