7e4b0390e55a5eb3d3dfddcd1a304603a2af96f0
Abdo <abdo@abdnh.net>
2024-05-16 12:57:03 +0300

    Fix mypy error

--- ankihub/gui/operations/ankihub_sync.py
+++ ankihub/gui/operations/ankihub_sync.py
@@ -174,7 +174,7 @@ def _upload_if_full_sync_triggered_by_ankihub(
             server_usn = out.server_media_usn if mw.pm.media_syncing_enabled() else None
             aqt.sync.full_upload(mw, server_usn, on_done)
         else:
-            aqt.sync.full_upload(mw, on_done)
+            aqt.sync.full_upload(mw, on_done)  # type: ignore
         config.set_schema_to_do_full_upload_for_once(None)
     else:
         _old(mw, out, on_done)
