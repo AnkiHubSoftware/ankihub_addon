764cb1304e02cea73d102e0f99d2be573128c030
RisingOrange <jakub.fidler@protonmail.com>
2024-05-23 17:12:39 +0200

    Refactor

--- ankihub/__init__.py
+++ ankihub/__init__.py
@@ -11,25 +11,6 @@ sys.path.insert(0, str(lib_other))
 
 import structlog
 
-shared_processors = [
-    structlog.stdlib.filter_by_level,
-    structlog.stdlib.add_log_level,
-    structlog.stdlib.PositionalArgumentsFormatter(),
-    structlog.processors.TimeStamper(fmt="iso"),
-    structlog.processors.StackInfoRenderer(),
-    structlog.processors.format_exc_info,
-    structlog.processors.UnicodeDecoder(),
-]
-
-structlog.configure(
-    processors=shared_processors
-    + [
-        structlog.stdlib.ProcessorFormatter.wrap_for_formatter,
-    ],
-    logger_factory=structlog.stdlib.LoggerFactory(),
-    cache_logger_on_first_use=True,
-)
-
 LOGGER = structlog.stdlib.get_logger("ankihub")
 
 SKIP_INIT = os.getenv("SKIP_INIT", False)
