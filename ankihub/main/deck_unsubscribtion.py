e2dc8b2379769efc73a1c78c5d88a726ee9aad83
RisingOrange <jakub.fidler@protonmail.com>
2024-05-24 16:28:28 +0200

    Adjust logs in deck_unsubscription.py

--- ankihub/main/deck_unsubscribtion.py
+++ ankihub/main/deck_unsubscribtion.py
@@ -11,7 +11,7 @@ def unsubscribe_from_deck_and_uninstall(ah_did: uuid.UUID) -> None:
     client = AddonAnkiHubClient()
     client.unsubscribe_from_deck(ah_did)
     uninstall_deck(ah_did)
-    LOGGER.info(f"Unsubscribed from deck {ah_did}")
+    LOGGER.info("Unsubscribed from deck.", ah_did=ah_did)
 
 
 def uninstall_deck(ah_did: uuid.UUID) -> None:
@@ -19,4 +19,4 @@ def uninstall_deck(ah_did: uuid.UUID) -> None:
     mids = ankihub_db.note_types_for_ankihub_deck(ah_did)
     undo_note_type_modfications(mids)
     ankihub_db.remove_deck(ah_did)
-    LOGGER.info(f"Uninstalled deck {ah_did}")
+    LOGGER.info("Uninstalled deck.", ah_did=ah_did)
