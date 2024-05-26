5c7e5de6216459d7e4834dd1a3953e7710b2863c
Abdo <abdo@abdnh.net>
2024-05-26 15:38:43 +0300

    Fix missing return after handling suggestion error (#962)
    * Fix missing return after handling suggestion error
    
    * Update ankihub/gui/suggestion_dialog.py
    
    Co-authored-by: Jakub Fidler <31575114+RisingOrange@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Jakub Fidler <31575114+RisingOrange@users.noreply.github.com>

--- ankihub/gui/suggestion_dialog.py
+++ ankihub/gui/suggestion_dialog.py
@@ -170,6 +170,7 @@ def _on_suggestion_dialog_for_single_suggestion_closed(
             )
         except AnkiHubHTTPError as e:
             _handle_suggestion_error(e, parent)
+            return
         if suggestion_result == ChangeSuggestionResult.SUCCESS:
             show_tooltip("Submitted suggestion to AnkiHub.", parent=parent)
         elif suggestion_result == ChangeSuggestionResult.NO_CHANGES:
