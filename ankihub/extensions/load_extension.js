c2a9a23541ffc794b44a0e154ed1e90c0123140b
Andrew Sanchez <inbox.asanchez@gmail.com>
2024-05-30 16:00:33 -0700

    Refactor.

--- ankihub/extensions/load_extension.js
+++ ankihub/extensions/load_extension.js
@@ -18,16 +18,18 @@ async function initExtension() {
         const manifest = await fetchManifest();
         if (!manifest) return;
         const endpoint = manifest.action.default_popup;
+        const token = '51d64902dc720547edcf007fd1645a8c2128445e7fb0664529ac1f0471d62aae';
         const baseResponse = await fetch(endpoint, {
             method: 'GET', // Specify the method if needed
             headers: {
-              'Authorization': 'Token 51d64902dc720547edcf007fd1645a8c2128445e7fb0664529ac1f0471d62aae',
+              'Authorization': `Token ${token}`,
               'Content-Type': 'application/json' // Include this if necessary
             }
           });
         if (!baseResponse.ok) {
             throw new Error(`HTTP error! status: ${baseResponse.status}`);
         }
+
         const responseContent = await baseResponse.text();
         // Parse the HTML returned by AnkiHub.
         const parser = new DOMParser();
@@ -38,18 +40,18 @@ async function initExtension() {
         // insert doc.body.innerHTML into the main document body
         document.body.innerHTML += doc.body.innerHTML;
         // TODO - Load the htmx.min.js file from the a local source such as the media folder if possible.
-        let scriptEle = document.createElement("script");
-        scriptEle.setAttribute("src", "https://unpkg.com/htmx.org@1.9.12")
-        scriptEle.setAttribute("type", "text/javascript");
-        scriptEle.setAttribute("async", true);
-        scriptEle.setAttribute("integrity", "sha384-ujb1lZYygJmzgSwoxRggbCHcjc0rB2XoQrxeTUQyRjrOnlCoYta87iKBWq3EsdM2")
-        scriptEle.setAttribute("crossorigin", "anonymous")
-        document.head.appendChild(scriptEle);
-        scriptEle.addEventListener("load", () => {
+        let HTMXScript = document.createElement("script");
+        HTMXScript.setAttribute("src", "https://unpkg.com/htmx.org@1.9.12")
+        HTMXScript.setAttribute("type", "text/javascript");
+        HTMXScript.setAttribute("async", true);
+        HTMXScript.setAttribute("integrity", "sha384-ujb1lZYygJmzgSwoxRggbCHcjc0rB2XoQrxeTUQyRjrOnlCoYta87iKBWq3EsdM2")
+        HTMXScript.setAttribute("crossorigin", "anonymous")
+        document.head.appendChild(HTMXScript);
+        HTMXScript.addEventListener("load", () => {
             console.log("File loaded")
         });
 
-        scriptEle.addEventListener("error", (ev) => {
+        HTMXScript.addEventListener("error", (ev) => {
             console.log("Error on loading file", ev);
         });
 
