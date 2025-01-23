f50bb4d2b166e8878063cb4d688c66364b62cbfb
Abdo <abdo@abdnh.net>
2025-01-23 19:47:10 +0300

    Force Windows button layout

--- ankihub/gui/utils.py
+++ ankihub/gui/utils.py
@@ -318,6 +318,8 @@ class _Dialog(QDialog):
 
     def _setup_button_box(self) -> QDialogButtonBox:
         button_box = QDialogButtonBox()
+        # Use Windows layout
+        button_box.setStyleSheet("button-layout: 0")
 
         self.default_button = None
         for button_index, button in enumerate(self.buttons):
