bc98ba78d4a10c66ef69f39d183c30c2d31399f0
Jakub Fidler <31575114+RisingOrange@users.noreply.github.com>
2025-02-18 16:55:09 +0100

    chore: Update mashumaro to 3.15 (#1097)
    * chore: Update mashumar to 3.15
    
    * Fix type hint

--- ankihub/lib/mashumaro/core/const.py
+++ ankihub/lib/mashumaro/core/const.py
@@ -1,32 +1,27 @@
+import enum
 import sys
 
-PY_36 = sys.version_info.major == 3 and sys.version_info.minor == 6
-PY_37 = sys.version_info.major == 3 and sys.version_info.minor == 7
-PY_38 = sys.version_info.major == 3 and sys.version_info.minor == 8
+__all__ = [
+    "PY_39",
+    "PY_310",
+    "PY_310_MIN",
+    "PY_311_MIN",
+    "PY_312_MIN",
+    "PY_313_MIN",
+    "Sentinel",
+]
+
+
 PY_39 = sys.version_info.major == 3 and sys.version_info.minor == 9
 PY_310 = sys.version_info.major == 3 and sys.version_info.minor == 10
 PY_311 = sys.version_info.major == 3 and sys.version_info.minor == 11
+PY_312 = sys.version_info.major == 3 and sys.version_info.minor == 12
+PY_313_MIN = sys.version_info.major == 3 and sys.version_info.minor >= 13
 
-PY_311_MIN = PY_311
+PY_312_MIN = PY_312 or PY_313_MIN
+PY_311_MIN = PY_311 or PY_312_MIN
 PY_310_MIN = PY_310 or PY_311_MIN
-PY_39_MIN = PY_39 or PY_310_MIN
-PY_38_MIN = PY_38 or PY_39_MIN
-PY_37_MIN = PY_37 or PY_38_MIN
-
-PEP_585_COMPATIBLE = PY_39_MIN  # Type Hinting Generics In Standard Collections
-PEP_586_COMPATIBLE = PY_38_MIN  # Literal Types
 
 
-__all__ = [
-    "PY_36",
-    "PY_37",
-    "PY_38",
-    "PY_39",
-    "PY_310",
-    "PY_37_MIN",
-    "PY_38_MIN",
-    "PY_39_MIN",
-    "PY_310_MIN",
-    "PEP_585_COMPATIBLE",
-    "PEP_586_COMPATIBLE",
-]
+class Sentinel(enum.Enum):
+    MISSING = enum.auto()
