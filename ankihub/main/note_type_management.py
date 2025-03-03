5dd52aa7b06005586a001fef8cbe5a999f05cd2b
Jakub Fidler <31575114+RisingOrange@users.noreply.github.com>
2025-03-03 12:21:54 +0100

    fix+ref: note_types_with_template_changes_for_deck (#1111)
    * Fix note_types_with_template_changes_for_deck, undo_note_type_modfications
    
    * Refactor, update test
    
    * ref: Rename db_note_type to ah_note_type

--- ankihub/main/note_type_management.py
+++ ankihub/main/note_type_management.py
@@ -1,6 +1,5 @@
-import copy
 import uuid
-from typing import Any, Dict, List
+from typing import List
 
 import aqt
 from anki.models import NotetypeDict, NotetypeId
@@ -8,11 +7,7 @@ from anki.models import NotetypeDict, NotetypeId
 from ..addon_ankihub_client import AddonAnkiHubClient
 from ..db import ankihub_db
 from ..settings import ANKIHUB_NOTE_TYPE_FIELD_NAME
-from .utils import (
-    ANKIHUB_CSS_END_COMMENT_PATTERN,
-    ANKIHUB_HTML_END_COMMENT_PATTERN,
-    modified_note_type,
-)
+from .utils import modified_note_type, note_type_without_ankihub_modifications
 
 
 def add_note_type(ah_did: uuid.UUID, note_type: NotetypeDict) -> NotetypeDict:
@@ -39,12 +34,12 @@ def add_note_type_fields(
 ) -> NotetypeDict:
     client = AddonAnkiHubClient()
 
-    db_note_type = ankihub_db.note_type_dict(ah_did, note_type["id"])
+    ah_note_type = ankihub_db.note_type_dict(ah_did, note_type["id"])
     new_fields = [
         field for field in note_type["flds"] if field["name"] in new_field_names
     ]
-    db_note_type["flds"].extend(new_fields)
-    for db_field in db_note_type["flds"]:
+    ah_note_type["flds"].extend(new_fields)
+    for db_field in ah_note_type["flds"]:
         field = next(
             (field for field in note_type["flds"] if field["name"] == db_field["name"]),
             None,
@@ -54,52 +49,42 @@ def add_note_type_fields(
     ankihub_id_field_idx = next(
         (
             idx
-            for idx, field in enumerate(db_note_type["flds"])
+            for idx, field in enumerate(ah_note_type["flds"])
             if field["name"] == ANKIHUB_NOTE_TYPE_FIELD_NAME
         ),
         None,
     )
     if ankihub_id_field_idx is not None:
-        db_note_type["flds"][ankihub_id_field_idx]["ord"] = (
-            len(db_note_type["flds"]) - 1
+        ah_note_type["flds"][ankihub_id_field_idx]["ord"] = (
+            len(ah_note_type["flds"]) - 1
         )
-        ankihub_id_field = db_note_type["flds"].pop(ankihub_id_field_idx)
-        db_note_type["flds"].append(ankihub_id_field)
-    db_note_type = client.update_note_type(ah_did, db_note_type, ["flds"])
-    ankihub_db.upsert_note_type(ankihub_did=ah_did, note_type=db_note_type)
+        ankihub_id_field = ah_note_type["flds"].pop(ankihub_id_field_idx)
+        ah_note_type["flds"].append(ankihub_id_field)
+    ah_note_type = client.update_note_type(ah_did, ah_note_type, ["flds"])
+    ankihub_db.upsert_note_type(ankihub_did=ah_did, note_type=ah_note_type)
 
-    return db_note_type
-
-
-def note_type_with_ankihub_end_comment_removed(
-    note_type: Dict[str, Any]
-) -> Dict[str, Any]:
-    note_type = copy.deepcopy(note_type)
-    note_type["css"] = ANKIHUB_CSS_END_COMMENT_PATTERN.sub("", note_type["css"])
-    for template in note_type["tmpls"]:
-        template["qfmt"] = ANKIHUB_HTML_END_COMMENT_PATTERN.sub("", template["qfmt"])
-        template["afmt"] = ANKIHUB_HTML_END_COMMENT_PATTERN.sub("", template["afmt"])
-
-    return note_type
+    return ah_note_type
 
 
 def note_types_with_template_changes_for_deck(ah_did: uuid.UUID) -> List[NotetypeId]:
     ids = []
     for mid in ankihub_db.note_types_for_ankihub_deck(ah_did):
         changed = False
-        db_note_type = note_type_with_ankihub_end_comment_removed(
+        ah_note_type = note_type_without_ankihub_modifications(
             ankihub_db.note_type_dict(ah_did, mid)
         )
-        note_type = note_type_with_ankihub_end_comment_removed(
+        anki_note_type = note_type_without_ankihub_modifications(
             aqt.mw.col.models.get(mid)
         )
-        if note_type["css"] != db_note_type["css"]:
+        if anki_note_type["css"] != ah_note_type["css"]:
             changed = True
-        elif len(note_type["tmpls"]) != len(db_note_type["tmpls"]):
+        elif len(anki_note_type["tmpls"]) != len(ah_note_type["tmpls"]):
             changed = True
         else:
-            for i, tmpl in enumerate(note_type["tmpls"]):
-                if tmpl != db_note_type["tmpls"][i]:
+            for anki_tmpl, ah_tmpl in zip(
+                anki_note_type["tmpls"], ah_note_type["tmpls"]
+            ):
+                if anki_tmpl != ah_tmpl:
                     changed = True
                     break
         if changed:
@@ -118,13 +103,13 @@ def update_note_type_templates_and_styles(
     ah_did: uuid.UUID, note_type: NotetypeDict
 ) -> NotetypeDict:
     client = AddonAnkiHubClient()
-    note_type = note_type_with_ankihub_end_comment_removed(note_type)
-    db_note_type = ankihub_db.note_type_dict(ah_did, note_type["id"])
+    note_type = note_type_without_ankihub_modifications(note_type)
+    ah_note_type = ankihub_db.note_type_dict(ah_did, note_type["id"])
 
-    db_note_type["tmpls"] = note_type["tmpls"]
-    db_note_type["css"] = note_type["css"]
+    ah_note_type["tmpls"] = note_type["tmpls"]
+    ah_note_type["css"] = note_type["css"]
 
-    db_note_type = client.update_note_type(ah_did, db_note_type, ["css", "tmpls"])
-    ankihub_db.upsert_note_type(ankihub_did=ah_did, note_type=db_note_type)
+    ah_note_type = client.update_note_type(ah_did, ah_note_type, ["css", "tmpls"])
+    ankihub_db.upsert_note_type(ankihub_did=ah_did, note_type=ah_note_type)
 
-    return db_note_type
+    return ah_note_type
