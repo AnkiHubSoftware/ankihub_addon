8d554cb4c28e3bc3efe47d76853e47425a2d5c01
Jakub Fidler <31575114+RisingOrange@users.noreply.github.com>
2025-05-26 16:48:52 +0200

    [NRT-168] Periodically recommend optimizing FSRS (#1148)
    * Add optimize_fsrs_params
    
    * Handle already optimal params
    
    * Clean up
    
    * Show dialog on Anki startup
    
    * ref: Reorder functions
    
    * Update reminder preference when checkbox is checked
    
    * Log user choice
    
    * Fix issues
    
    * Use paramSearch if available
    
    * Increae min anki version for FSRS features to 24.11
    
    * Fix compute_fsrs_params parameters
    
    * Fix issues with dialog on mac
    
    * Uncomment lines
    
    * Add on_done callback
    
    * Add test_optimize_fsrs_parameters
    
    * Parametrize test
    
    * Fix issues in gui/deck_options.py
    
    * Fix tests
    
    * Add test_maybe_show_fsrs_optimization_reminder
    
    * Add test_show_fsrs_optimization_reminder_skip_and_dont_show_again
    
    * Add test_deck_configs_for_update_exposes_fsrs_fields

---
+++ ankihub/gui/deck_options.py
@@ -0,0 +1,200 @@
+from concurrent.futures import Future
+from datetime import datetime, timezone
+from typing import Callable, Optional
+
+import aqt
+from anki import scheduler_pb2
+from anki.decks import DeckConfigDict, DeckConfigId
+from aqt.qt import QCheckBox, QDialogButtonBox
+from aqt.utils import tooltip
+
+from .. import LOGGER
+from ..main.deck_options import get_fsrs_parameters
+from ..settings import ANKI_INT_VERSION, FSRS_VERSION, config
+from .utils import show_dialog
+
+# Minimum Anki version for which the AnkiHub add-on provides FSRS features
+MIN_ANKI_VERSION_FOR_FSRS_FEATURES = 241100
+
+FSRS_OPTIMIZATION_REMINDER_INTERVAL_DAYS = 30
+
+
+def maybe_show_fsrs_optimization_reminder() -> None:
+    if not config.get_feature_flags().get("fsrs_reminder", False):
+        return
+
+    deck_config = config.deck_config(config.anking_deck_id)
+    if not deck_config:
+        return
+
+    anki_did = deck_config.anki_id
+    deck_configs_for_update = aqt.mw.col.decks.get_deck_configs_for_update(anki_did)
+    if (
+        config.public_config["remind_to_optimize_fsrs_parameters"]
+        and ANKI_INT_VERSION >= MIN_ANKI_VERSION_FOR_FSRS_FEATURES
+        and deck_configs_for_update.fsrs
+        # days_since_last_fsrs_optimize is a global value, not just for the current deck, but that's okay, because
+        # if the user optimized the parameters for some deck, they probably don't need the reminder
+        and deck_configs_for_update.days_since_last_fsrs_optimize
+        >= FSRS_OPTIMIZATION_REMINDER_INTERVAL_DAYS
+    ):
+        _show_fsrs_optimization_reminder()
+
+
+def _show_fsrs_optimization_reminder() -> None:
+    deck_config = config.deck_config(config.anking_deck_id)
+    if not deck_config:
+        return
+
+    anki_did = deck_config.anki_id
+    if aqt.mw.col.decks.get(anki_did) is None:
+        return
+
+    def on_button_clicked(button_idx: Optional[int]) -> None:
+        optimize = button_idx == 1
+
+        assert isinstance(dialog.dont_show_this_again_cb, QCheckBox)
+        dont_show_again = dialog.dont_show_this_again_cb.isChecked()
+
+        LOGGER.info(
+            "fsrs_optimization_reminder_dialog_choice",
+            user_choice="optimize" if optimize else "skip",
+            dont_show_again=dont_show_again,
+        )
+
+        if dont_show_again:
+            config.public_config["remind_to_optimize_fsrs_parameters"] = False
+            config.save_public_config()
+
+        if not optimize:
+            return
+
+        if aqt.mw.col.decks.get(anki_did) is None:
+            return
+
+        conf_id = aqt.mw.col.decks.config_dict_for_deck_id(anki_did)["id"]
+        optimize_fsrs_parameters(conf_id)
+
+    dialog = show_dialog(
+        text="""
+            <h3>🛠️ Keep Your FSRS Scheduler Optimized</h3>
+            <p>To keep your reviews efficient, AnKing recommends optimizing your
+            <a href="https://docs.ankiweb.net/deck-options.html#fsrs">FSRS</a>
+            (Free Spaced Repetition Scheduler) parameters monthly.</p>
+            <p>You can always undo changes by clicking “Revert to Previous Parameters” in the deck settings.</p>
+            <p>⚠️ <strong>Prevent data loss:</strong> make sure all your other devices are synced
+            with AnkiWeb before proceeding.
+            </p>
+            <p><strong>Would you like us to optimize the AnKing FSRS parameters for you?</strong></p>
+            """,
+        title="AnKing Recommendation",
+        buttons=[
+            ("Skip", QDialogButtonBox.ButtonRole.RejectRole),
+            ("Optimize", QDialogButtonBox.ButtonRole.AcceptRole),
+        ],
+        default_button_idx=1,
+        callback=on_button_clicked,
+        open_dialog=False,
+        add_title_to_body_on_mac=False,
+        parent=aqt.mw,
+    )
+
+    dialog.dont_show_this_again_cb = QCheckBox("Don't show this again")
+    layout = dialog.content_layout
+    layout.insertWidget(
+        layout.count() - 2,
+        dialog.dont_show_this_again_cb,
+    )
+    dialog.adjustSize()
+
+    dialog.show()
+
+
+def optimize_fsrs_parameters(
+    conf_id: DeckConfigId, on_done: Optional[Callable[[], None]] = None
+) -> None:
+    deck_config = aqt.mw.col.decks.get_config(conf_id)
+
+    _, fsrs_parameters = get_fsrs_parameters(conf_id)
+
+    def compute_fsrs_params() -> scheduler_pb2.ComputeFsrsParamsResponse:
+        deck_config_name_escaped = (
+            deck_config["name"].replace("\\", "\\\\").replace('"', '\\"')
+        )
+        default_search = f'preset:"{deck_config_name_escaped}" -is:suspended'
+
+        ignore_revlog_before_date_str = (
+            deck_config.get("ignoreRevlogsBeforeDate") or "1970-01-01"
+        )
+        ignore_revlog_before_date = datetime.fromisoformat(
+            ignore_revlog_before_date_str
+        ).replace(tzinfo=timezone.utc)
+        ignore_revlog_before_ms = int(ignore_revlog_before_date.timestamp() * 1000)
+
+        extra_kwargs = {}
+        if ANKI_INT_VERSION >= 250200:
+            # The num_of_relearning_steps parameter became available in Anki 25.02
+            extra_kwargs[
+                "num_of_relearning_steps"
+            ] = _get_amount_relearning_steps_in_day(
+                deck_config,
+            )
+
+        return aqt.mw.col.backend.compute_fsrs_params(
+            search=deck_config.get(
+                "paramSearch", deck_config.get("weightSearch", default_search)
+            ),
+            current_params=fsrs_parameters,
+            ignore_revlogs_before_ms=ignore_revlog_before_ms,
+            **extra_kwargs,
+        )
+
+    def on_compute_fsrs_params_done(future: Future) -> None:
+        response: scheduler_pb2.ComputeFsrsParamsResponse = future.result()
+        new_parameters = list(response.params)
+
+        old_parameters = list(fsrs_parameters)
+        parameters_are_the_same = len(new_parameters) == len(old_parameters) and all(
+            round(new_param, 4) == round(old_param, 4)
+            for new_param, old_param in zip(new_parameters, old_parameters)
+        )
+        already_optimal = not new_parameters or parameters_are_the_same
+
+        if already_optimal:
+            aqt.mw.taskman.run_on_main(
+                lambda: tooltip("FSRS parameters are already optimal!", parent=aqt.mw)
+            )
+        else:
+            deck_config = aqt.mw.col.decks.get_config(conf_id)
+            deck_config[f"fsrsParams{FSRS_VERSION}"] = new_parameters
+            aqt.mw.col.decks.update_config(deck_config)
+
+            aqt.mw.taskman.run_on_main(
+                lambda: tooltip(
+                    "FSRS parameters optimized successfully!", parent=aqt.mw
+                )
+            )
+
+        if on_done:
+            on_done()
+
+    aqt.mw.taskman.with_progress(
+        task=compute_fsrs_params,
+        on_done=on_compute_fsrs_params_done,
+        label="Optimizing FSRS parameters",
+        parent=aqt.mw,
+    )
+
+
+def _get_amount_relearning_steps_in_day(deck_config: DeckConfigDict) -> int:
+    # Ported from TS code
+    # https://github.com/ankitects/anki/blob/main/ts/routes/deck-options/FsrsOptions.svelte
+    num_of_relearning_steps_in_day = 0
+    accumulated_time = 0
+    for step in deck_config["lapse"]["delays"]:
+        accumulated_time += step
+        if accumulated_time >= 24 * 60:  # minutes in a day
+            break
+        num_of_relearning_steps_in_day += 1
+
+    return num_of_relearning_steps_in_day
