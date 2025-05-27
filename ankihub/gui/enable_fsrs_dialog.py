b053e3e71d6c60801cd46b9addc17ea3eb0ba760
daniel carvalho <dnc331@gmail.com>
2025-05-26 22:30:51 -0300

    Create maybe_show_enable_fsrs_reminder  method and dialog

---
+++ ankihub/gui/enable_fsrs_dialog.py
@@ -0,0 +1,94 @@
+from datetime import date
+from typing import Optional
+
+import aqt
+
+from .. import LOGGER
+from ..settings import ANKI_INT_VERSION, config
+from .deck_options import MIN_ANKI_VERSION_FOR_FSRS_FEATURES
+from .utils import show_dialog
+
+ENABLE_FSRS_REMINDER_INTERVAL = 30
+
+
+def maybe_show_enable_fsrs_reminder():
+    if ANKI_INT_VERSION < MIN_ANKI_VERSION_FOR_FSRS_FEATURES:
+        return
+
+    if not config.get_feature_flags().get("fsrs_reminder", False):
+        return
+
+    anking_deck_id = config.anking_deck_id
+    if not bool(config.deck_config(anking_deck_id)):
+        return
+
+    if aqt.mw.col.get_config("fsrs"):
+        return
+
+    if not config._private_config.show_enable_fsrs_remind_again:
+        return
+
+    last_remind_date = config._private_config.last_enable_fsrs_remind_date
+    today_date = date.today()
+    if (
+        last_remind_date is None
+        or (today_date - last_remind_date).days >= ENABLE_FSRS_REMINDER_INTERVAL
+    ):
+        config.set_last_enable_fsrs_remind_date(today_date)
+        _show_enable_fsrs_reminder()
+
+
+def _show_enable_fsrs_reminder() -> None:
+    def on_button_clicked(button_idx: Optional[int]) -> None:
+        enable = button_idx == 1
+
+        assert isinstance(dialog.dont_show_this_again_cb, aqt.QCheckBox)
+        dont_show_again = dialog.dont_show_this_again_cb.isChecked()
+
+        LOGGER.info(
+            "enable_fsrs_reminder_dialog_choice",
+            user_choice="enable" if enable else "skip",
+            dont_show_again=dont_show_again,
+        )
+
+        if dont_show_again:
+            config.set_show_enable_fsrs_remind_again(False)
+            return
+
+        if not enable:
+            return
+
+        aqt.mw.col.set_config("fsrs", True)
+
+    dialog = show_dialog(
+        text="""
+            <h3>⚙️ Enable FSRS for Smarter Reviews</h3>
+            <p><a href="https://docs.ankiweb.net/deck-options.html#fsrs">FSRS</a>
+            (Free Spaced Repetition Scheduler) is a new,
+            smarter scheduling system <strong>recommended by The AnKing.</strong></p>
+            <p>It adapts to your learning pace and customizes your review intervals
+            — helping you study less and remember more.</p>
+
+            <p><strong>Want us to enable FSRS for you for all decks?</strong></p>
+            """,
+        title="AnKing Recommendation",
+        buttons=[
+            ("Skip", aqt.QDialogButtonBox.ButtonRole.RejectRole),
+            ("Enable FSRS", aqt.QDialogButtonBox.ButtonRole.AcceptRole),
+        ],
+        default_button_idx=1,
+        callback=on_button_clicked,
+        open_dialog=False,
+        add_title_to_body_on_mac=False,
+        parent=aqt.mw,
+    )
+
+    dialog.dont_show_this_again_cb = aqt.QCheckBox("Don't show this again")
+    layout = dialog.content_layout
+    layout.insertWidget(
+        layout.count() - 2.5,
+        dialog.dont_show_this_again_cb,
+    )
+    dialog.adjustSize()
+
+    dialog.show()
