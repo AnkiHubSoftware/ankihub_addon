72a6ac5a3c047b8e56925fb197dfa9667132a3a9
Jakub Fidler <31575114+RisingOrange@users.noreply.github.com>
2025-10-08 20:40:38 +0200

    [NRT-420] Fix user name/id getting cleared from config (#1198)
    * Fix user name/id getting cleared from config
    
    * Fetch user details and flags when Anki profile is changed
    
    * ref: Rename _fetch_user_details
    
    * Update log message

--- ankihub/gui/operations/user_details.py
+++ ankihub/gui/operations/user_details.py
@@ -9,17 +9,24 @@ from ...gui.operations import AddonQueryOp
 from ...settings import config
 
 
-def _fetch_user_details_in_background() -> None:
+def _fetch_user_details() -> None:
+    if not config.is_logged_in():
+        config.save_username("")
+        config.save_user_id(None)
+        return
+
+    if config.username() and config.user_id():
+        return
+
     username = ""
     user_id = None
-    if config.is_logged_in() and (not config.username() or not config.user_id()):
-        client = AnkiHubClient()
-        try:
-            user_details = client.get_user_details()
-            username = user_details["username"]
-            user_id = user_details["id"]
-        except (AnkiHubRequestException, AnkiHubHTTPError) as exc:
-            LOGGER.warning(f"Failed to fetch user details: {exc}")
+    client = AnkiHubClient()
+    try:
+        user_details = client.get_user_details()
+        username = user_details["username"]
+        user_id = user_details["id"]
+    except (AnkiHubRequestException, AnkiHubHTTPError) as exc:
+        LOGGER.warning(f"Failed to fetch user details: {exc}")
     config.save_username(username)
     config.save_user_id(user_id)
 
@@ -30,7 +37,7 @@ def fetch_user_details_in_background() -> None:
 
     AddonQueryOp(
         parent=aqt.mw,
-        op=lambda _: _fetch_user_details_in_background(),
+        op=lambda _: _fetch_user_details(),
         success=on_done,
     ).without_collection().run_in_background()
 
