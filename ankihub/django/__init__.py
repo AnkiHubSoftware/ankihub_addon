3b5ff869cccb91d3aa6f946f750eb3cb7978849f
Abdo <abdo@abdnh.net>
2025-12-23 15:47:58 +0300

    Add a function to render a string and allow passing context

--- ankihub/django/__init__.py
+++ ankihub/django/__init__.py
@@ -1,8 +1,10 @@
 from collections import OrderedDict
+from typing import Any, Dict, Optional
 
 from django.apps import apps
 from django.conf import settings
 from django.template import engines
+from django_cotton.cotton_loader import CottonCompiler
 
 from ..settings import ADDON_PATH
 
@@ -37,10 +39,19 @@ apps.app_configs = OrderedDict()
 apps.ready = False
 apps.populate(settings.INSTALLED_APPS)
 engine = engines["django"]
+cotton_compiler = CottonCompiler()
 
 
-def render_template(name: str) -> str:
+def render_template(name: str, context: Optional[Dict[str, Any]] = None) -> str:
     template = engine.get_template(name)
-    text = template.render()
+    text = template.render(context=context)
+
+    return text
+
+
+def render_template_from_string(html: str, context: Optional[Dict[str, Any]] = None) -> str:
+    html = cotton_compiler.process(html)
+    template = engine.from_string(html)
+    text = template.render(context=context)
 
     return text
