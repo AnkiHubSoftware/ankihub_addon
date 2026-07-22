a83c766b03bb468d56965166aefb4d10a8104f51
Abdo <abdo@abdnh.net>
2026-07-22 06:33:45 +0300

    Use a separate exception for AnkiWeb

--- ankihub/ankihub_client/ankiweb_client.py
+++ ankihub/ankihub_client/ankiweb_client.py
@@ -5,6 +5,8 @@ Only enabled on Python 3.10+ due to the protobuf-py requirement.
 
 from typing import TYPE_CHECKING
 
+from requests import Response
+
 from ..proto.account_pb import (
     LoginRequest,
     LoginResponse,
@@ -21,10 +23,20 @@ from ..proto.account_pb import (
     SignupRequest,
     SignupResponse,
 )
-from .ankihub_client import API, AnkiHubHTTPError
+from .ankihub_client import API
 
-if TYPE_CHECKING:
-    from requests import Response
+
+class AnkiWebHTTPError(Exception):
+    """An unexpected HTTP code was returned in response to a request by the AnkiWeb client."""
+
+    def __init__(self, response: Response):
+        self.response = response
+
+    def __str__(self):
+        try:
+            return self.response.text
+        except Exception:
+            return f"AnkiWeb responded with status error code {self.response.status_code}"
 
 
 class AnkiWebClientMixin:
@@ -49,42 +61,42 @@ class AnkiWebClientMixin:
         data = LoginRequest(username=username, password=password)
         response = self._send_request("post", API.ANKIWEB, "/account/login", data=data.to_binary())
         if response.status_code != 200:
-            raise AnkiHubHTTPError(response)
+            raise AnkiWebHTTPError(response)
         return LoginResponse.from_binary(response.content)
 
     def ankiweb_signup(self, username: str, password: str) -> SignupResponse:
         data = SignupRequest(username=username, password=password)
         response = self._send_request("post", API.ANKIWEB, "/account/signup", data=data.to_binary())
         if response.status_code != 200:
-            raise AnkiHubHTTPError(response)
+            raise AnkiWebHTTPError(response)
         return SignupResponse.from_binary(response.content)
 
     def ankiweb_request_signup_code(self, email: str, terms: bool) -> MagicCodeSignupResponse:
         data = MagicCodeSignupRequest(email=email, terms=terms)
         response = self._send_request("post", API.ANKIWEB, "/account/magic-code-signup", data=data.to_binary())
         if response.status_code != 200:
-            raise AnkiHubHTTPError(response)
+            raise AnkiWebHTTPError(response)
         return MagicCodeSignupResponse.from_binary(response.content)
 
     def ankiweb_verify_signup_code(self, email: str, code: str) -> MagicCodeSignupVerifyResponse:
         data = MagicCodeSignupVerifyRequest(email=email, code=code)
         response = self._send_request("post", API.ANKIWEB, "/account/magic-code-signup-verify", data=data.to_binary())
         if response.status_code != 200:
-            raise AnkiHubHTTPError(response)
+            raise AnkiWebHTTPError(response)
         return MagicCodeSignupVerifyResponse.from_binary(response.content)
 
     def ankiweb_request_login_code(self, email: str) -> MagicCodeLoginResponse:
         data = MagicCodeLoginRequest(email=email)
         response = self._send_request("post", API.ANKIWEB, "/account/magic-code-login", data=data.to_binary())
         if response.status_code != 200:
-            raise AnkiHubHTTPError(response)
+            raise AnkiWebHTTPError(response)
         return MagicCodeLoginResponse.from_binary(response.content)
 
     def ankiweb_verify_login_code(self, email: str, code: str) -> MagicCodeLoginVerifyResponse:
         data = MagicCodeLoginVerifyRequest(email=email, code=code)
         response = self._send_request("post", API.ANKIWEB, "/account/magic-code-login-verify", data=data.to_binary())
         if response.status_code != 200:
-            raise AnkiHubHTTPError(response)
+            raise AnkiWebHTTPError(response)
         return MagicCodeLoginVerifyResponse.from_binary(response.content)
 
     # FIXME: this only works with cookie authentication
@@ -92,5 +104,5 @@ class AnkiWebClientMixin:
         data = ResendVerificationRequest()
         response = self._send_request("post", API.ANKIWEB, "/account/resend-verification", data=data.to_binary())
         if response.status_code != 200:
-            raise AnkiHubHTTPError(response)
+            raise AnkiWebHTTPError(response)
         return ResendVerificationResponse.from_binary(response.content)
